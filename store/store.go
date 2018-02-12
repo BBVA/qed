d66c92466885e4a0bb1631e4cb84c6f56e8e21e4
Gabriel Díaz <g.diaz.lopezllave@bbva.com>
2018-02-12 15:48:06 +0100

    add store skel

---
+++ store/store.go
@@ -0,0 +1,8 @@
+// Copyright 2018 BBVA. All rights reserved.
+// Use of this source code is governed by a Apache 2 License
+//  that can be found in the LICENSE file
+
+/*
+	Package store implements a common API to access to multiple storage engines 
+*/
+package store
