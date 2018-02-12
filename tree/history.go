9256f6d773bad4d40f7825da5a9111037252ef73
Gabriel Díaz <g.diaz.lopezllave@bbva.com>
2018-02-12 15:48:13 +0100

    add tree skel

---
+++ tree/history.go
@@ -0,0 +1,6 @@
+// Copyright 2018 BBVA. All rights reserved.
+// Use of this source code is governed by a Apache 2 License
+//  that can be found in the LICENSE file
+
+package tree
+
---
+++ tree/tree.go
@@ -0,0 +1,8 @@
+// Copyright 2018 BBVA. All rights reserved.
+// Use of this source code is governed by a Apache 2 License
+//  that can be found in the LICENSE file
+
+/*
+	Package tree implements all the trees used in the system.
+*/
+package tree
