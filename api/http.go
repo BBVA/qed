ee992204918f9d03ee85fd7f14d6beee3ed18a73
Gabriel Díaz <g.diaz.lopezllave@bbva.com>
2018-02-12 16:30:57 +0100

    fix header format

--- api/http.go
+++ api/http.go
@@ -1,6 +1,6 @@
 // Copyright 2018 BBVA. All rights reserved.
 // Use of this source code is governed by a Apache 2 License
-//  that can be found in the LICENSE file.
+// that can be found in the LICENSE file.
 
 /*
 	Package API implements the interface to use the Verification Data Service
--- api/http_test.go
+++ api/http_test.go
@@ -1,6 +1,6 @@
 // Copyright 2018 BBVA. All rights reserved.
 // Use of this source code is governed by a Apache 2 License
-//  that can be found in the LICENSE file
+// that can be found in the LICENSE file
 
 package api
 
