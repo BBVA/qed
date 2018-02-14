8fb4bd271a62f423c7a431ae7642e88c982f8255
Gabriel Díaz <g.diaz.lopezllave@bbva.com>
2018-02-14 16:22:41 +0100

    add basic documentation to memory store

--- store/memory/memory.go
+++ store/memory/memory.go
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a Apache 2 License
 // that can be found in the LICENSE file
 
+
+// Package memory implements a Store compatible interface 
+// using sync.Map as a backend.
 package memory
 
 import (
@@ -11,15 +14,20 @@ import (
 	"verifiabledata/store"
 )
 
+
+// Contains unexported fields
 type MemoryStore struct {
 	store *sync.Map
 }
 
+// Implements Add store interface to add tree.Node to the store
 func (m *MemoryStore) Add(n tree.Node) error {
 	m.store.Store(n.Pos,n)
 	return nil
 }
 
+// Implements Get store interface to get tree.Node from the store
+// given a tree.Position
 func (m *MemoryStore) Get(p tree.Position) (*tree.Node, error) {
 	v, ok := m.store.Load(p)
 	if ! ok {
@@ -32,6 +40,8 @@ func (m *MemoryStore) Get(p tree.Position) (*tree.Node, error) {
 	return nil, fmt.Errorf("Error getting node from storage")
 }
 
+
+// Returns a new instance of a MemoryStore
 func NewMemoryStore() store.Store {
 	return &MemoryStore{
 		new(sync.Map),
