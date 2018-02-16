1f06f7e3ad49f7893ae8f5d13a164fe39dafa582
Gabriel Diaz <g.diaz.lopezllave@bbva.com>
2018-02-16 15:33:02 +0100

    update test code, add basic benchmark

--- tree/history/history_test.go
+++ tree/history/history_test.go
@@ -5,29 +5,32 @@
 package history
 
 import (
+	"crypto/rand"
 	"fmt"
 	"testing"
 	"verifiabledata/store/memory"
 )
 
-var eventTable = []struct {
-	index  uint64
-	commitment string
-	event  string
-}{
-	{0, "5cd26c62ee55c4a327fc7ec1eae97a232e7355f4340adfb0b3ca25b8d94135bd", "Hello World1"},
-	{0, "81d3aa6da152370015e028ef97e9d303ffbf7ae121e362059e66bd217d5e09ce", "Hello World2"},
-	{0, "0871c0d34eb2311a101cf1de957d15103c014885b1c306354766fbca2bc3d10e", "Hello World3"},
-	{0, "8e4d915dcdbe9fd485336ecb7fa6780fc901179c6c5ded78781661120f3e3365", "Hello World4"},
-	{0, "377f2fb38a02913effc8ec6de5bf51bfe1ebe2e473ea4fb5060f94b7c11b676e", "Hello World5"},
-}
+
 
 func TestAdd(t *testing.T) {
+	var testCases = []struct {
+		index      uint64
+		commitment string
+		event      string
+	}{
+		{0, "5cd26c62ee55c4a327fc7ec1eae97a232e7355f4340adfb0b3ca25b8d94135bd", "Hello World1"},
+		{0, "81d3aa6da152370015e028ef97e9d303ffbf7ae121e362059e66bd217d5e09ce", "Hello World2"},
+		{0, "0871c0d34eb2311a101cf1de957d15103c014885b1c306354766fbca2bc3d10e", "Hello World3"},
+		{0, "8e4d915dcdbe9fd485336ecb7fa6780fc901179c6c5ded78781661120f3e3365", "Hello World4"},
+		{0, "377f2fb38a02913effc8ec6de5bf51bfe1ebe2e473ea4fb5060f94b7c11b676e", "Hello World5"},
+	}
+
 	frozen := memory.NewMemoryStore()
 	events := memory.NewMemoryStore()
 	ht := NewHistoryTree(frozen, events)
 
-	for _, e := range eventTable {
+	for _, e := range testCases {
 		t.Log("Testing event: ", e.event)
 		node, err := ht.Add([]byte(e.event))
 		if err != nil {
@@ -40,6 +43,20 @@ func TestAdd(t *testing.T) {
 		commitment := fmt.Sprintf("%x", node.Digest)
 		if e.commitment != commitment {
 			t.Fatal("Incorrect commitment: ", e.commitment, " != ", commitment)
+		}
+	}
+}
+
+func BenchmarkAdd(b *testing.B) {
+	frozen := memory.NewMemoryStore()
+	events := memory.NewMemoryStore()
+	ht := NewHistoryTree(frozen, events)
+	data := make([]byte, 64)
+	rand.Read(data)
+	for i:=0;i<b.N;i++ {
+		_, err := ht.Add(data)
+		if err != nil {
+			b.Log(err)
 		}
 	}
 }
