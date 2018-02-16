0fa89f1a15f4a88d9538d13e8c544957d400a7b5
Gabriel Diaz <g.diaz.lopezllave@bbva.com>
2018-02-16 10:45:51 +0100

    add stringers to types, and change interfaces to use pointers

--- tree/tree.go
+++ tree/tree.go
@@ -8,27 +8,52 @@
 package tree
 
 import (
+	"fmt"
 	"verifiabledata/util"
 )
 
 // Data type holds the information of a leaf node
 type Data []byte
 
-// Commitment holds the Digest as proof of the event insertion, and the verstion of 
+// Commitment holds the Digest as proof of the event insertion, and the verstion of
 // the tree after the insertion, which is equivalent to the Position.Index
 type Commitment struct {
 	Version uint64
-	Digest util.Digest
+	Digest  util.Digest
 }
 
 // Position holds the index and the layer of a node in a tree
 type Position struct {
 	Index uint64
-	Layer uint
+	Layer uint64
+}
+
+func (p *Position) String() string {
+	return fmt.Sprintf("(i %d, l %d)",p.Index, p.Layer)
+}
+
+// Returns a copy of position with l as Layer
+func (p *Position) SetLayer(l uint64) *Position {
+	return &Position{
+		p.Index,
+		l,
+	}
+}
+
+// Returns a copy of position with i as Index
+func (p *Position) SetIndex(i uint64) *Position {
+	return &Position{
+		i,
+		p.Layer,
+	}
 }
 
 // A node holds its digest and its position
 type Node struct {
-	Pos Position
+	Pos    *Position
 	Digest util.Digest
+}
+
+func (n *Node) String() string {
+	return fmt.Sprintf("(P %s,  D %x)", n.Pos, n.Digest)
 }
