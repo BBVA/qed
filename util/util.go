869cbda00edebcc374c54a91bf817e9042a6121f
Gabriel Diaz <g.diaz.lopezllave@bbva.com>
2018-02-16 10:46:24 +0100

    change digest type to slice of bytes

--- util/util.go
+++ util/util.go
@@ -9,12 +9,18 @@ package util
 
 import (
 	"crypto/sha256"
+	"math"
 )
 
 // The return type of the hash function, in our case a [32]byte
-type Digest [sha256.Size]byte
+type Digest []byte
 
 // Returns a Digest of the []byte passed as a parameter
 func Hash(buff []byte) Digest {
-	return sha256.Sum256(buff)
+	d := sha256.Sum256(buff)
+	return Digest(d[:])
+}
+
+func Pow(x, y uint64) uint64 {
+	return uint64(math.Pow(float64(x), float64(y)))
 }
