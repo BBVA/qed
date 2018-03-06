61eaf95dcfab0555157e0de5b221e7b52c59c1a7
pancho horrillo <pedrofelipe.horrillo@bbva.com>
2018-03-06 16:02:30 +0100

    Fix typo in copyright notice

--- merkle/history/history.go
+++ merkle/history/history.go
@@ -1,4 +1,4 @@
-// Copyrigt2018 BBVA. All rights reserved.
+// Copyright 2018 BBVA. All rights reserved.
 // Use of this source code is governed by a Apache 2 License
 // that can be found in the LICENSE file
 
