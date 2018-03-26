586274d49faeb3532a264d2678d091535cb50f2e
iknite <enrique@iknite.com>
2018-03-26 12:59:19 +0200

    Store fetch events in a channel buffer and post it after tree.Add

--- cmd/agent.go
+++ cmd/agent.go
@@ -6,7 +6,7 @@ import (
 	"flag"
 	"os"
 	// "os/signal"
-	// "sync"
+	"sync"
 
 	"github.com/golang/glog"
 
@@ -16,23 +16,44 @@ import (
 func main() {
 	flag.Parse()
 
-	// var wg sync.WaitGroup
+	var wg sync.WaitGroup
 
 	ctx := context.Background()
 
 	glog.V(2).Info("Starting agent")
 
 	agent, err := agent.Run(ctx)
-
 	if err != nil {
 		defer os.Exit(255)
 		glog.Exitf("Agent exited with error: %v", err)
 	}
 
+	ch := make(chan string, 10000)
+	wg.Add(1)
+	// cl_ch := make(chan int)
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		agent.Add(scanner.Text())
+		ch <- scanner.Text()
 	}
+
+	go func() {
+		glog.Info("init channel")
+
+		for {
+			select {
+			case msg := <-ch:
+				agent.Fetch(msg)
+
+			default:
+				close(ch)
+				wg.Done()
+				return
+
+			}
+		}
+	}()
 
 	// c := make(chan os.Signal, 1)
 	// wg.Add(1)
@@ -40,10 +61,11 @@ func main() {
 	// signal.Notify(c, os.Interrupt)
 	// go func() {
 	// 	<-c
+	// 	wg.Done()
 	// 	defer os.Exit(1)
 	// 	close(c)
 	// }()
-	//
-	// wg.Wait()
+
+	wg.Wait()
 
 }
