007fbf6b3c51f1f7620f97162160e9bb2c364e28
pancho horrillo <pedrofelipe.horrillo@bbva.com>
2018-04-12 16:56:42 +0200

    Add copyright file to all *.go files

--- balloon/storage/cache/simple_cache.go
+++ balloon/storage/cache/simple_cache.go
@@ -1,3 +1,7 @@
+// Copyright © 2018 Banco Bilbao Vizcaya Argentaria S.A.  All rights reserved.
+// Use of this source code is governed by an Apache 2 License
+// that can be found in the LICENSE file
+
 package cache
 
 // SimpleCache is a cache that contains the hashes of the pre-computed nodes
--- cmd/agent/agent.go
+++ cmd/agent/agent.go
@@ -1,3 +1,7 @@
+// Copyright © 2018 Banco Bilbao Vizcaya Argentaria S.A.  All rights reserved.
+// Use of this source code is governed by an Apache 2 License
+// that can be found in the LICENSE file
+
 package main
 
 import (
