56c86e216c2f23be52eca05690bf476c5f32c5f7
iknite <enrique@iknite.com>
2018-04-12 11:36:07 +0200

    Fix typos

--- cmd/server.go
+++ cmd/server.go
@@ -11,15 +11,16 @@ import (
 	"net/http"
 	"verifiabledata/api/apihttp"
 	"verifiabledata/balloon/hashing"
+	"verifiabledata/balloon/storage"
 	"verifiabledata/balloon/storage/badger"
 	"verifiabledata/balloon/storage/bolt"
 )
 
 var (
-	httpEndpoint = flag.String("http_endpoint", "localhost:8080", "Endpoint for REST requests on (host:port)")
+	httpEndpoint = flag.String("http_endpoint", ":8080", "Endpoint for REST requests on (host:port)")
 	path         = flag.String("path", "/tmp/balloon.db", "Set default storage path.")
 	cache        = flag.Int64("cache", 5000000, "Initialize and reserve custom cache size.")
-	storage      = flag.String("storage", "badger", "Choose between different storage backends. Eg badge|bolt")
+	stor         = flag.String("storage", "badger", "Choose between different storage backends. Eg badge|bolt")
 )
 
 func main() {
@@ -28,7 +29,7 @@ func main() {
 
 	var frozen, leaves storage.Store
 
-	swtich storage {
+	switch stor {
 	case "badge":
 		frozen = badger.NewBadgerStorage(fmt.Sprintf("%s/frozen.db", path))
 		leaves = badger.NewBadgerStorage(fmt.Sprintf("%s/leaves.db", path))
@@ -41,7 +42,7 @@ func main() {
 
 	balloon := NewBalloon(path, hashing.Sha256Hasher, frozen, leaves, cache)
 
-	err := http.ListenAndServe(":8080", apihttp.New(balloon))
+	err := http.ListenAndServe(httpEndpoint, apihttp.New(balloon))
 	if err != nil {
 		log.Fatalln("Can't start HTTP Server: ", err)
 	}
