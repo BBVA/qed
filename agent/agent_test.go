6ef634229b1dab804151b9884aeab41c513c4df5
iknite <enrique@iknite.com>
2018-04-16 13:21:55 +0200

    Add internal server load

--- agent/agent_test.go
+++ agent/agent_test.go
@@ -5,10 +5,39 @@
 package agent
 
 import (
+	"fmt"
+	"log"
+	"net/http"
 	"testing"
+
+	"verifiabledata/api/apihttp"
+	"verifiabledata/balloon"
+	"verifiabledata/balloon/hashing"
+	"verifiabledata/balloon/storage/badger"
+	"verifiabledata/balloon/storage/cache"
 )
 
+func dummyServer() {
+
+	dbPath := "/tmp/testAdd"
+	frozen := badger.NewBadgerStorage(fmt.Sprintf("%s/frozen.db", dbPath))
+	leaves := badger.NewBadgerStorage(fmt.Sprintf("%s/leaves.db", dbPath))
+	cache := cache.NewSimpleCache(5000000)
+	balloon := balloon.NewHyperBalloon(dbPath, hashing.Sha256Hasher, frozen, leaves, cache)
+
+	err := http.ListenAndServe(":8080", apihttp.New(balloon))
+	if err != nil {
+		log.Fatalln("Can't start HTTP Server: ", err)
+	}
+}
+
+var flag bool
+
 func TestAdd(t *testing.T) {
+	if !flag {
+		go dummyServer()
+		flag = true
+	}
 
 	testAgent, _ := NewAgent("http://localhost:8080")
 	testAgent.Add("Hola mundo!")
@@ -16,6 +45,11 @@ func TestAdd(t *testing.T) {
 }
 
 func BenchmarkAdd(b *testing.B) {
+
+	if !flag {
+		go dummyServer()
+		flag = true
+	}
 
 	testAgent, _ := NewAgent("http://localhost:8080")
 
