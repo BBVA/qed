9d78e9e8c50c4e7275935fee1b256150343655c0
pancho horrillo <pedrofelipe.horrillo@bbva.com>
2018-04-17 10:04:09 +0200

    hash.go: Export LeafHasherF and Interior HasherF

--- balloon/hyper/hash.go
+++ balloon/hyper/hash.go
@@ -14,7 +14,7 @@ var Set = []byte{0x01}
 type LeafHasher func([]byte, []byte, []byte) []byte
 type InteriorHasher func([]byte, []byte, []byte, []byte) []byte
 
-func leafHasherF(hasher hashing.Hasher) LeafHasher {
+func LeafHasherF(hasher hashing.Hasher) LeafHasher {
 	return func(id, a, base []byte) []byte {
 		if bytes.Equal(a, Empty) {
 			return hasher(id)
@@ -23,7 +23,7 @@ func leafHasherF(hasher hashing.Hasher) LeafHasher {
 	}
 }
 
-func interiorHasherF(hasher hashing.Hasher) InteriorHasher {
+func InteriorHasherF(hasher hashing.Hasher) InteriorHasher {
 	return func(left, right, base, height []byte) []byte {
 		if bytes.Equal(left, right) {
 			return hasher(left, right)
