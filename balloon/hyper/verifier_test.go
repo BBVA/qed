b3c5ebc872d0b91d971ebadd755fb41252ee3d25
Alvaro Alda <alvaro.alda@bbva.com>
2018-04-19 16:29:09 +0200

    Add test to ensure whole add-prove-verify cycle

--- balloon/hyper/verifier_test.go
+++ balloon/hyper/verifier_test.go
@@ -6,8 +6,10 @@ package hyper
 
 import (
 	"bytes"
+	"fmt"
 	"testing"
 	"verifiabledata/balloon/hashing"
+	"verifiabledata/balloon/storage/cache"
 )
 
 func TestVerify(t *testing.T) {
@@ -38,4 +40,39 @@ func TestVerify(t *testing.T) {
 	if bytes.Compare(recomputed, expectedCommitment) != 0 {
 		t.Fatalf("Expected: %x, Actual: %x", expectedCommitment, recomputed)
 	}
+}
+
+func TestAddAndVerify(t *testing.T) {
+
+	store, closeF := openBPlusStorage()
+	defer closeF()
+
+	hasher := hashing.Sha256Hasher
+	ht := NewTree(string(0x0), 30, cache.NewSimpleCache(5000000), store, hasher, LeafHasherF(hasher), InteriorHasherF(hasher))
+	verifier := NewVerifier(string(0x0), hasher, LeafHasherF(hasher), InteriorHasherF(hasher))
+
+	key := hasher([]byte("test event 1"))
+	value := hasher([]byte{0})
+
+	commitment := <-ht.Add(key, value)
+	proof := <-ht.Prove(key)
+
+	if !bytes.Equal(proof.ActualValue, value) {
+		fmt.Errorf("Wrong proof: expected value %v , actual %v", value, proof.ActualValue)
+	}
+
+	fullPath := make([][]byte, len(proof.AuditPath)+1)
+	copy(fullPath[:1], [][]byte{key})
+	copy(fullPath[1:], proof.AuditPath)
+
+	isMembership, recomputed := verifier.Verify(commitment, fullPath, key, value)
+
+	if !isMembership {
+		fmt.Errorf("Key %v should be a member", key)
+	}
+
+	if !bytes.Equal(recomputed, commitment) {
+		fmt.Errorf("Commitments don't match: expected %v , actual %v", commitment, recomputed)
+	}
+
 }
