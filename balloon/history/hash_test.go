41250f259401407e4f618d7a04acb7c05185e543
Gabriel Diaz <g.diaz.lopezllave@bbvva.com>
2018-04-20 12:59:23 +0200

    add clean hashers to test easier

--- balloon/history/hash_test.go
+++ balloon/history/hash_test.go
@@ -1,20 +1,32 @@
+// Copyright © 2018 Banco Bilbao Vizcaya Argentaria S.A.  All rights reserved.
+// Use of this source code is governed by an Apache 2 License
+// that can be found in the LICENSE file
 package history
 
 import (
-	"fmt"
 	"verifiabledata/balloon/hashing"
 )
 
 func fakeLeafHasherF(hasher hashing.Hasher) LeafHasher {
 	return func(a, key []byte) []byte {
-		fmt.Printf("Hashing leaf: a-> %b key-> %b\n", a, key)
 		return hasher(a, key)
 	}
 }
 
 func fakeInteriorHasherF(hasher hashing.Hasher) InteriorHasher {
 	return func(a, left, right []byte) []byte {
-		fmt.Printf("Hashing interior: a-> %b left-> %b right-> %b\n", a, left, right)
 		return hasher(a, left, right)
+	}
+}
+
+func fakeLeafHasherCleanF(hasher hashing.Hasher) LeafHasher {
+	return func(a, key []byte) []byte {
+		return hasher(key)
+	}
+}
+
+func fakeInteriorHasherCleanF(hasher hashing.Hasher) InteriorHasher {
+	return func(a, left, right []byte) []byte {
+		return hasher(left, right)
 	}
 }
