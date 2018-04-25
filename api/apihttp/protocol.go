09e069344ba7c6de07daaa61ec95190d565c2ac3
Gabriel Diaz <g.diaz.lopezllave@bbvva.com>
2018-04-25 17:52:20 +0200

    refactor types, let the json encode the []bytes to base64

--- api/apihttp/protocol.go
+++ api/apihttp/protocol.go
@@ -1,81 +1,72 @@
 package apihttp
 
 import (
-	"fmt"
 	"verifiabledata/balloon"
 	"verifiabledata/balloon/history"
 )
 
 type Snapshot struct {
-	HyperDigest   string
-	HistoryDigest string
+	HyperDigest   []byte
+	HistoryDigest []byte
 	Version       uint
-	Event         string
+	Event         []byte
 	//TODO: implement this
 	// EventDigest   string
 }
 
 type HistoryNode struct {
-	Digest       string
+	Digest       []byte
 	Index, Layer uint
 }
 
 type Proofs struct {
-	HyperAuditPath   []string
+	HyperAuditPath   [][]byte
 	HistoryAuditPath []HistoryNode
 }
 
 type MembershipProof struct {
-	Key                         string
-	KeyDigest                   string
+	Key                         []byte
+	KeyDigest                   []byte
 	IsMember                    bool
 	Proofs                      *Proofs
 	QueryVersion, ActualVersion uint
 }
 
-func assemblySnapshot(commitment *balloon.Commitment, event string) *Snapshot {
+func ToSnapshot(commitment *balloon.Commitment, event string) *Snapshot {
 	return &Snapshot{
-		fmt.Sprintf("%064x", commitment.HyperDigest),
-		fmt.Sprintf("%064x", commitment.HistoryDigest),
+		commitment.HyperDigest,
+		commitment.HistoryDigest,
 		commitment.Version,
-		event,
+		[]byte(event),
 	}
 }
 
-func assemblyHyperAuditPath(path [][]byte) []string {
-	result := make([]string, 0)
-	for _, elem := range path {
-		result = append(result, fmt.Sprintf("%064x", elem))
-	}
-	return result
-}
-
-func assemblyHistoryAuditPath(path []history.Node) []HistoryNode {
+func ToHistoryAuditPath(path []history.Node) []HistoryNode {
 	result := make([]HistoryNode, 0)
 	for _, elem := range path {
-		result = append(result, assemblyHistoryNode(elem))
+		result = append(result, HistoryNode{elem.Digest, elem.Index, elem.Layer})
 	}
 	return result
 }
 
-func assemblyHistoryNode(node history.Node) HistoryNode {
-	return HistoryNode{
-		fmt.Sprintf("%064x", node.Digest),
-		node.Index,
-		node.Layer,
-	}
-}
-
-func assemblyMembershipProof(event string, proof *balloon.MembershipProof) *MembershipProof {
+func ToMembershipProof(event string, proof *balloon.MembershipProof) *MembershipProof {
 	return &MembershipProof{
-		event,
-		fmt.Sprintf("%064x", proof.KeyDigest),
+		[]byte(event),
+		proof.KeyDigest,
 		proof.Exists,
 		&Proofs{
-			assemblyHyperAuditPath(proof.HyperProof),
-			assemblyHistoryAuditPath(proof.HistoryProof),
+			proof.HyperProof,
+			ToHistoryAuditPath(proof.HistoryProof),
 		},
 		proof.QueryVersion,
 		proof.ActualVersion,
 	}
+}
+
+func ToHistoryNode(path []HistoryNode) []history.Node {
+	result := make([]history.Node, 0)
+	for _, elem := range path {
+		result = append(result, history.Node{elem.Digest, elem.Index, elem.Layer})
+	}
+	return result
 }
