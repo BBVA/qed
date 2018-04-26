6ba20de4bcdb0e8e700b1993cb8ed6542f2b7107
Alvaro Alda <alvaro.alda@bbva.com>
2018-04-26 15:40:36 +0200

    Integrate client into cli
    Former-commit-id: f9dfcc4b34726b17356f974d94f4838b158281ae

---
+++ cli/context.go
@@ -0,0 +1,28 @@
+package cli
+
+import (
+	"log"
+	"os"
+	"verifiabledata/client"
+)
+
+// Context is used to create varios objects across the project and is being passed
+// to every command as a constructor argument.
+type Context struct {
+	logger   *log.Logger
+	client   *client.HttpClient
+	endpoint string
+	apikey   string
+}
+
+func NewContext() *Context {
+	return &Context{}
+}
+
+// Logger returns the CLI logger.
+func (ctx *Context) Logger() *log.Logger {
+	if ctx.logger == nil {
+		ctx.logger = log.New(os.Stdout, "QED", log.Ldate|log.Ltime|log.Lmicroseconds|log.Llongfile)
+	}
+	return ctx.logger
+}
