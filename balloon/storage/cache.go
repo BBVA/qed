ee9af6b3160c86db063a8eccef6c8c4c88992a67
Gabriel Diaz <g.diaz.lopezllave@bbvva.com>
2018-04-30 17:31:15 +0200

    add 25 level cache size
    Former-commit-id: 3646543c796276444dc9e66d42d4bc96f3c72b0b

--- balloon/storage/cache.go
+++ balloon/storage/cache.go
@@ -4,11 +4,14 @@
 
 package storage
 
-// INFO: 2^30 -1 == 1073741823 nodes, 30 levels of cache
-const SIZE30 = 1073741823
+// INFO: 2^30 == 1073741824 nodes, 30 levels of cache
+const SIZE30 = 1073741824
 
-// INFO: 2^20 -1 == 1048574 nodes, 20 levels of cache
-const SIZE20 = 1048574
+// INFO: 2^20 == 1048575 nodes, 20 levels of cache
+const SIZE20 = 1048575
+
+// INFO: 2^25 == 33554432 nodes, 25 levels of cache
+const SIZE25 = 33554432
 
 // Cache interface defines the operations a cache mechanism must implement to
 // be usable within the tree
