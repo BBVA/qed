3bb7b4dddae39a415d92af2b0f73ff85499f12ca
Gabriel Diaz <g.diaz.lopezllave@bbva.com>
2018-05-03 16:35:24 +0200

    add logging to the example server
    Former-commit-id: 3b5500b735e1574c1e75d26756d1b721a33c7d9a

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -7,8 +7,9 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"verifiabledata/api/apihttp"
 	"verifiabledata/balloon"
@@ -19,11 +20,12 @@ import (
 	"verifiabledata/balloon/storage/badger"
 	"verifiabledata/balloon/storage/bolt"
 	"verifiabledata/balloon/storage/cache"
+	"verifiabledata/log"
 )
 
 var (
-	httpEndpoint, dbPath, storageName string
-	cacheSize                         uint64
+	logLevel, httpEndpoint, dbPath, storageName string
+	cacheSize                                   uint64
 )
 
 func main() {
@@ -32,9 +34,11 @@ func main() {
 	flag.StringVar(&dbPath, "path", "/tmp/balloon.db", "Set default storage path.")
 	flag.Uint64Var(&cacheSize, "cache", storage.SIZE25, "Initialize and reserve custom cache size.")
 	flag.StringVar(&storageName, "storage", "badger", "Choose between different storage backends. Eg badge|bolt")
+	flag.StringVar(&logLevel, "log", "silent", "Choose between log levels: silent, error, info and debug")
 	flag.Parse()
 
 	var frozen, leaves storage.Store
+	var l log.Logger
 
 	switch storageName {
 	case "badger":
@@ -45,16 +49,72 @@ func main() {
 		leaves = bolt.NewBoltStorage(fmt.Sprintf("%s/leaves.db", dbPath), "leaves")
 	default:
 		fmt.Print("Please select a valid storage backend")
+		os.Exit(-1)
+	}
+
+	switch logLevel {
+	case "silent":
+		l = log.NewSilent()
+	case "error":
+		l = log.NewError(os.Stdout, "Server: ", log.Ldate|log.Ltime|log.Lmicroseconds|log.Llongfile)
+	case "info":
+		l = log.NewInfo(os.Stdout, "Server: ", log.Ldate|log.Ltime|log.Lmicroseconds|log.Llongfile)
+	case "debug":
+		l = log.NewDebug(os.Stdout, "Server: ", log.Ldate|log.Ltime|log.Lmicroseconds|log.Llongfile)
+	default:
+		fmt.Print("Please select a valid log level")
+		os.Exit(-1)
 	}
 
 	cache := cache.NewSimpleCache(cacheSize)
 	hasher := hashing.Sha256Hasher
 	history := history.NewTree(frozen, hasher)
 	hyper := hyper.NewTree(dbPath, cache, leaves, hasher)
-	balloon := balloon.NewHyperBalloon(hasher, history, hyper)
+	balloon := balloon.NewHyperBalloon(hasher, history, hyper, l)
 
-	err := http.ListenAndServe(httpEndpoint, apihttp.NewApiHttp(balloon))
+	router := apihttp.NewApiHttp(balloon)
+
+	s := &http.Server{
+		Addr:    httpEndpoint,
+		Handler: logHandler(router, l),
+	}
+
+	err := s.ListenAndServe()
 	if err != nil {
-		log.Fatalln("Can't start HTTP Server: ", err)
+		l.Errorf("Can't start HTTP Server: ", err)
+	}
+}
+
+type statusWriter struct {
+	http.ResponseWriter
+	status int
+	length int
+}
+
+func (w *statusWriter) WriteHeader(status int) {
+	w.status = status
+	w.ResponseWriter.WriteHeader(status)
+}
+
+func (w *statusWriter) Write(b []byte) (int, error) {
+	if w.status == 0 {
+		w.status = 200
+	}
+	w.length = len(b)
+	return w.ResponseWriter.Write(b)
+}
+
+// WriteLog Logs the Http Status for a request into fileHandler and returns a httphandler function which is a wrapper to log the requests.
+func logHandler(handle http.Handler, l log.Logger) http.HandlerFunc {
+	return func(w http.ResponseWriter, request *http.Request) {
+		start := time.Now()
+		writer := statusWriter{w, 0, 0}
+		handle.ServeHTTP(&writer, request)
+		latency := time.Now().Sub(start)
+
+		l.Debugf("Request: lat %d %+v", latency, request)
+		if writer.status >= 400 {
+			l.Infof("Bad Request: %d %+v", latency, request)
+		}
 	}
 }
