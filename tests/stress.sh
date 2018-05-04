fc1b34d723ff23c4e67adf880b14b9a1c2033e72
Martin Evgeniev <martin.evgeniev@bbva.com>
2018-05-04 13:15:10 +0200

    Force Vegeta concurrency and increase stress time.
    Former-commit-id: e8dccd30716eb14128c3d1bd34b41449a419d174

--- tests/stress.sh
+++ tests/stress.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-vegeta attack -duration=30s -targets=stress/targets.txt | vegeta report -reporter='hist[0,2ms,4ms,6ms]'
+vegeta attack -connections 1 -workers 1 -rate 100 -duration=60s -targets=stress/targets.txt | vegeta report -reporter='hist[10ms,100ms,500ms,1s,10s,1m]'
