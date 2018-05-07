0f0dd07c499dc28f2699a355fc99f0b223bcf3c8
pancho horrillo <pedrofelipe.horrillo@bbva.com>
2018-05-07 13:04:24 +0200

    Whitespace fixes
    Former-commit-id: e9274f3bb2ea617dd1d1251fdc89e62622816f00

--- tests/stress.sh
+++ tests/stress.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-vegeta attack -connections 1 -workers 1 -rate 1500 -duration=10s -targets=stress/targets.txt > result.bin 
+vegeta attack -connections 1 -workers 1 -rate 1500 -duration=10s -targets=stress/targets.txt > result.bin
 cat result.bin | vegeta report -reporter='hist[10ms,100ms,500ms,1s,10s,1m]'
 cat result.bin | vegeta report -reporter='plot' > plot.html
