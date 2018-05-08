596221fbea0a38b7c7683da1a68f9a032892dcf0
Gabriel Diaz <g.diaz.lopezllave@bbva.com>
2018-05-08 15:57:54 +0200

    add more options to tune the attack

--- tests/attack_add.go
+++ tests/attack_add.go
@@ -13,16 +13,18 @@ import (
 )
 
 func main() {
-
-	duration := flag.Duration("d", 10*time.Second, "es la duration")
-	endpoint := flag.String("e", "http://localhost:8080/events", "es el endpoint")
-	apikey := flag.String("k", "apikey", "es el apikey")
+	conn := flag.Int("c", 1, "request per second")
+	workers := flag.Uint64("w", 1, "request per second")
+	timeout := flag.Duration("t", 30*time.Second, "timeout")
+	duration := flag.Duration("d", 10*time.Second, "duration")
+	endpoint := flag.String("e", "http://localhost:8080/events", "endpoint")
+	apikey := flag.String("k", "apikey", "apikey")
 	rate := flag.Uint64("r", 100, "request per second")
 	flag.Parse()
 
 	targeter := myTargeter(*endpoint, http.Header{"Api-Key": []string{*apikey}})
 
-	atk := vegeta.NewAttacker()
+	atk := vegeta.NewAttacker(vegeta.Connections(*conn), vegeta.Workers(*workers), vegeta.Timeout(*timeout))
 	res := atk.Attack(targeter, *rate, *duration)
 	enc := vegeta.NewEncoder(os.Stdout)
 	sig := make(chan os.Signal, 1)
