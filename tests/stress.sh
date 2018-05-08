62b744d026f8831623766f43025588270dc9721c
iknite <enrique@iknite.com>
2018-05-08 14:46:40 +0200

    Use attack_add.go in stress.sh
    Former-commit-id: bc6a906bdff302191cc34c4832bb9c069c0e0833

--- tests/stress.sh
+++ tests/stress.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
-echo Clean old data
-rm /tmp/targets.txt
+# echo Clean old data
+# rm /tmp/targets.txt
+#
+# echo Generate event json
+# for i in {1..100000}; do echo -e "{\"message\": \"Load Test $i\"}" > /tmp/event$i.json;done
+#
+# echo Generate targets file
+# for i in {1..100000}; do echo -e "POST http://localhost:8080/events\nContent-Type: application/json\nApi-key: this-is-my-dummy-key\n@/tmp/event$i.json\n" >> /tmp/targets.txt;done
+#
+# echo Execute Vegeta benchmark
+# vegeta attack -timeout 100s -rate 1500 -duration=60s -targets=/tmp/targets.txt > result.bin
 
-echo Generate event json
-for i in {1..100000}; do echo -e "{\"message\": \"Load Test $i\"}" > /tmp/event$i.json;done
+go run attack_add.go -k key -r 250 -d 120s > result.bin
 
-echo Generate targets file
-for i in {1..100000}; do echo -e "POST http://localhost:8080/events\nContent-Type: application/json\nApi-key: this-is-my-dummy-key\n@/tmp/event$i.json\n" >> /tmp/targets.txt;done
-
-echo Execute Vegeta benchmark
-vegeta attack -timeout 100s -rate 1500 -duration=60s -targets=/tmp/targets.txt > result.bin 
 cat result.bin | vegeta report
 cat result.bin | vegeta report -reporter='plot' > plot.html
