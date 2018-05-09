4476a3d0f4c8dea3c4794dc88a31cd4c54aad8c3
pancho horrillo <pedrofelipe.horrillo@bbva.com>
2018-05-09 16:13:52 +0200

    Remove simplified copyright headers

--- api/api_test.go
+++ api/api_test.go
@@ -1,7 +1,3 @@
-// Copyright © 2018 Banco Bilbao Vizcaya Argentaria S.A.  All rights reserved.
-// Use of this source code is governed by an Apache 2 License
-// that can be found in the LICENSE file
-
 /*
 	Package API implements the interface to use the Verification Data Service
 */
--- balloon/history/historytest.go
+++ balloon/history/historytest.go
@@ -1,6 +1,3 @@
-// Copyright © 2018 Banco Bilbao Vizcaya Argentaria S.A.  All rights reserved.
-// Use of this source code is governed by an Apache 2 License
-// that can be found in the LICENSE file
 package history
 
 import (
