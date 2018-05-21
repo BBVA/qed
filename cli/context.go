5862526f91ae3303d195395d3d74972656c544c2
pancho horrillo <pedrofelipe.horrillo@bbva.com>
2018-05-21 11:54:35 +0200

    Fix typos
    Former-commit-id: d7f15a04c5bc730b98695a6408f14a8500246142

--- cli/context.go
+++ cli/context.go
@@ -20,7 +20,7 @@ import (
 	"github.com/bbva/qed/client"
 )
 
-// Context is used to create varios objects across the project and is being passed
+// Context is used to create various objects across the project and is being passed
 // to every command as a constructor argument.
 type Context struct {
 	client *client.HttpClient
