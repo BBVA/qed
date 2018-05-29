a201a2aa87067074c6e998c620ba09db16c5df68
pancho horrillo <pedrofelipe.horrillo@bbva.com>
2018-05-29 11:51:29 +0200

    cmd/root.go: Remove empty line
    Co-authored-by: Martin Evgeniev <suizman@outlook.com>

--- cmd/root.go
+++ cmd/root.go
@@ -61,7 +61,6 @@ func NewRootCommand(ctx *Context) *cobra.Command {
 	cmd.AddCommand(newStartCommand())
 	cmd.AddCommand(newClientCommand(ctx))
 	cmd.AddCommand(newAuditorCommand(ctx))
-
 	cmd.AddCommand(newAddCommand(ctx))
 	cmd.AddCommand(newMembershipCommand(ctx))
 
