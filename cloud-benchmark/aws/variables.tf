6251832e3e6336712d6ef19149a39babad8a2a82
Martin Evgeniev <martin.evgeniev@bbva.com>
2018-06-07 17:34:45 +0200

    Bring up basic AWS instance with Terraform
    flavour, region, volume_size are arbitrary parameters.

---
+++ cloud-benchmark/aws/variables.tf
@@ -0,0 +1,24 @@
+/*
+   Copyright 2018 Banco Bilbao Vizcaya Argentaria, S.A.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+// Force choose instance size.
+variable "flavour" {}
+
+// Force choose region.
+variable "region" {}
+
+// Force choose root volume size for our instance.
+variable "volume_size" {}
