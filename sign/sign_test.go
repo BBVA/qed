39e8c2de80dfddd08226f055212defab83d65398
iknite <enrique@iknite.com>
2018-07-04 14:12:44 +0200

    Add read openssh key to signer and pass along as param to server

--- sign/sign_test.go
+++ sign/sign_test.go
@@ -17,10 +17,7 @@ func testSign(t *testing.T, signer Signable) {
 	assert.True(t, result, "Must be verified")
 
 }
-func TestNilSign(t *testing.T)   { testSign(t, NewNilSigner()) }
-func TestRsaSign(t *testing.T)   { testSign(t, NewRSASigner(2048)) }
-func TestEdSign(t *testing.T)    { testSign(t, NewEdSigner()) }
-func TestEcdsaSign(t *testing.T) { testSign(t, NewEcdsaSigner()) }
+func TestEdSign(t *testing.T) { testSign(t, NewSigner()) }
 
 func syncBenchmark(b *testing.B, signer Signable, iterations int) {
 
@@ -59,51 +56,17 @@ func asyncBenchmark(b *testing.B, signer Signable, numRoutines, iterations int) 
 
 }
 
-func BenchmarkRSA(b *testing.B) {
-
-	for _, rsaSize := range []int{2048, 4096} {
-		for _, iterations := range []int{1e3, 1e4, 1e5} {
-			for _, numRoutines := range []int{1e1, 1e2, 1e3} {
-				b.Run(fmt.Sprintf("size-%d_routines-%d", rsaSize, numRoutines), func(b *testing.B) {
-					asyncBenchmark(b, NewRSASigner(rsaSize), numRoutines, iterations)
-				})
-			}
-
-			b.Run(fmt.Sprintf("size-%d", rsaSize), func(b *testing.B) {
-				syncBenchmark(b, NewRSASigner(rsaSize), iterations)
-			})
-		}
-	}
-
-}
-
 func BenchmarkEd(b *testing.B) {
 
 	for _, iterations := range []int{1e3, 1e4, 1e5} {
 		for _, numRoutines := range []int{1e1, 1e2, 1e3} {
 			b.Run(fmt.Sprintf("routines-%d", numRoutines), func(b *testing.B) {
-				asyncBenchmark(b, NewEdSigner(), numRoutines, iterations)
+				asyncBenchmark(b, NewSigner(), numRoutines, iterations)
 			})
 		}
 
 		b.Run(fmt.Sprintf(""), func(b *testing.B) {
-			syncBenchmark(b, NewEdSigner(), iterations)
-		})
-	}
-
-}
-
-func BenchmarkEcdsa(b *testing.B) {
-
-	for _, iterations := range []int{1e3, 1e4, 1e5} {
-		for _, numRoutines := range []int{1e1, 1e2, 1e3} {
-			b.Run(fmt.Sprintf("routines-%d", numRoutines), func(b *testing.B) {
-				asyncBenchmark(b, NewEcdsaSigner(), numRoutines, iterations)
-			})
-		}
-
-		b.Run(fmt.Sprintf(""), func(b *testing.B) {
-			syncBenchmark(b, NewEcdsaSigner(), iterations)
+			syncBenchmark(b, NewSigner(), iterations)
 		})
 	}
 
