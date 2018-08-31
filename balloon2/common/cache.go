63623b3a4ccca77a0d3216b3f1b69c1d021268d9
Alvaro Alda <alvaro.alda@bbva.com>
2018-08-31 11:53:19 +0200

    Add cache implementations
    Former-commit-id: 7aed0bfcad64a97de7410f92804a12e18eb74c28 [formerly 4018a7768dd8c2b2f692a423dc1a36ad625dff35]
    Former-commit-id: 0db359e0460a447d080fa9f81e3e91a90d4ad891

--- balloon2/common/cache.go
+++ balloon2/common/cache.go
@@ -1,5 +1,9 @@
 package common
 
+import (
+	"github.com/bbva/qed/db"
+)
+
 type Cache interface {
 	Get(pos Position) (Digest, bool)
 }
@@ -7,4 +11,43 @@ type Cache interface {
 type ModifiableCache interface {
 	Put(pos Position, value Digest)
 	Cache
+}
+type PassThroughCache struct {
+	prefix byte
+	store  db.Store
+}
+
+func NewPassThroughCache(prefix byte, store db.Store) *PassThroughCache {
+	return &PassThroughCache{prefix, store}
+}
+
+func (c PassThroughCache) Get(pos Position) (Digest, bool) {
+	pair, err := c.store.Get(c.prefix, pos.Bytes())
+	if err != nil {
+		return nil, false
+	}
+	return pair.Value, true
+}
+
+const keySize = 34
+
+type SimpleCache struct {
+	cached map[[keySize]byte]Digest
+}
+
+func NewSimpleCache(initialSize uint64) *SimpleCache {
+	return &SimpleCache{make(map[[keySize]byte]Digest, initialSize)}
+}
+
+func (c SimpleCache) Get(pos Position) (Digest, bool) {
+	var key [keySize]byte
+	copy(key[:], pos.Bytes())
+	digest, ok := c.cached[key]
+	return digest, ok
+}
+
+func (c *SimpleCache) Put(pos Position, value Digest) {
+	var key [keySize]byte
+	copy(key[:], pos.Bytes())
+	c.cached[key] = value
 }
