abe0c7b7aead7584cc637e9c577a90e5d2915b9d
Alvaro Alda <alvaro.alda@bbva.com>
2018-08-31 11:18:43 +0200

    Add balloon's verify test
    Former-commit-id: 362f82e1e2d54a8e4e2bae9b9d327ad6217dad30

--- balloon2/common/audit.go
+++ balloon2/common/audit.go
@@ -6,3 +6,24 @@ func (p AuditPath) Get(pos Position) (Digest, bool) {
 	digest, ok := p[pos.StringId()]
 	return digest, ok
 }
+
+type Verifiable interface {
+	Verify(expectedDigest Digest, key, value []byte) bool
+	AuditPath() AuditPath
+}
+
+type FakeVerifiable struct {
+	result bool
+}
+
+func NewFakeVerifiable(result bool) *FakeVerifiable {
+	return &FakeVerifiable{result}
+}
+
+func (f FakeVerifiable) Verify(commitment Digest, key, value []byte) bool {
+	return f.result
+}
+
+func (f FakeVerifiable) AuditPath() AuditPath {
+	return make(AuditPath)
+}
