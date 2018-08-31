f8fac7ed7859d8c917dbc2451ba0f10240064c7f
Alvaro Alda <alvaro.alda@bbva.com>
2018-08-31 13:19:48 +0200

    Add audit path visitor

--- balloon2/common/audit.go
+++ balloon2/common/audit.go
@@ -1,5 +1,7 @@
 package common
 
+import "github.com/aalda/trees/log"
+
 type AuditPath map[string]Digest
 
 func (p AuditPath) Get(pos Position) (Digest, bool) {
@@ -26,4 +28,49 @@ func (f FakeVerifiable) Verify(commitment Digest, key, value []byte) bool {
 
 func (f FakeVerifiable) AuditPath() AuditPath {
 	return make(AuditPath)
+}
+
+type AuditPathVisitor struct {
+	decorated *ComputeHashVisitor
+	auditPath AuditPath
+}
+
+func NewAuditPathVisitor(decorated *ComputeHashVisitor) *AuditPathVisitor {
+	return &AuditPathVisitor{decorated, make(AuditPath)}
+}
+
+func (v AuditPathVisitor) Result() AuditPath {
+	return v.auditPath
+}
+
+func (v *AuditPathVisitor) VisitRoot(pos Position, leftResult, rightResult interface{}) interface{} {
+	// by-pass
+	return v.decorated.VisitRoot(pos, leftResult, rightResult)
+}
+
+func (v *AuditPathVisitor) VisitNode(pos Position, leftResult, rightResult interface{}) interface{} {
+	// by-pass
+	return v.decorated.VisitNode(pos, leftResult, rightResult)
+}
+
+func (v *AuditPathVisitor) VisitPartialNode(pos Position, leftResult interface{}) interface{} {
+	// by-pass
+	return v.decorated.VisitPartialNode(pos, leftResult)
+}
+
+func (v *AuditPathVisitor) VisitLeaf(pos Position, eventDigest []byte) interface{} {
+	// ignore. target leafs not included in path
+	return v.decorated.VisitLeaf(pos, eventDigest)
+}
+
+func (v *AuditPathVisitor) VisitCached(pos Position, cachedDigest Digest) interface{} {
+	// by-pass
+	return v.decorated.VisitCached(pos, cachedDigest)
+}
+
+func (v *AuditPathVisitor) VisitCollectable(pos Position, result interface{}) interface{} {
+	digest := v.decorated.VisitCollectable(pos, result)
+	log.Debugf("Adding collectable to path in position: %v", pos)
+	v.auditPath[pos.StringId()] = digest.(Digest)
+	return digest
 }
---
+++ balloon2/common/audit_test.go
@@ -0,0 +1,87 @@
+package common
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestAuditPathVisitor(t *testing.T) {
+
+	testCases := []struct {
+		visitable         Visitable
+		expectedAuditPath AuditPath
+	}{
+		{
+			visitable:         NewLeaf(&FakePosition{[]byte{0x0}, 0}, []byte{0x1}),
+			expectedAuditPath: AuditPath{},
+		},
+		{
+			visitable: NewRoot(
+				&FakePosition{[]byte{0x0}, 1},
+				NewCached(&FakePosition{[]byte{0x0}, 0}, Digest{0x0}),
+				NewLeaf(&FakePosition{[]byte{0x1}, 0}, Digest{0x1}),
+			),
+			expectedAuditPath: AuditPath{},
+		},
+		{
+			visitable: NewRoot(
+				&FakePosition{[]byte{0x0}, 2},
+				NewCached(&FakePosition{[]byte{0x0}, 1}, Digest{0x1}),
+				NewPartialNode(&FakePosition{[]byte{0x1}, 1},
+					NewLeaf(&FakePosition{[]byte{0x2}, 0}, Digest{0x2}),
+				),
+			),
+			expectedAuditPath: AuditPath{},
+		},
+		{
+			visitable: NewRoot(
+				&FakePosition{[]byte{0x0}, 2},
+				NewCached(&FakePosition{[]byte{0x0}, 1}, Digest{0x1}),
+				NewNode(&FakePosition{[]byte{0x1}, 1},
+					NewCached(&FakePosition{[]byte{0x2}, 0}, Digest{0x2}),
+					NewLeaf(&FakePosition{[]byte{0x3}, 0}, Digest{0x3}),
+				),
+			),
+			expectedAuditPath: AuditPath{},
+		},
+		{
+			visitable: NewRoot(
+				&FakePosition{[]byte{0x0}, 2},
+				NewCollectable(&FakePosition{[]byte{0x0}, 1},
+					NewCached(&FakePosition{[]byte{0x0}, 1}, Digest{0x1})),
+				NewPartialNode(&FakePosition{[]byte{0x1}, 1},
+					NewLeaf(&FakePosition{[]byte{0x2}, 0}, Digest{0x2}),
+				),
+			),
+			expectedAuditPath: AuditPath{
+				"00|1": Digest{0x1},
+			},
+		},
+		{
+			visitable: NewRoot(
+				&FakePosition{[]byte{0x0}, 2},
+				NewCollectable(&FakePosition{[]byte{0x0}, 1},
+					NewCached(&FakePosition{[]byte{0x0}, 1}, Digest{0x1})),
+				NewNode(&FakePosition{[]byte{0x1}, 1},
+					NewCollectable(&FakePosition{[]byte{0x2}, 0},
+						NewCached(&FakePosition{[]byte{0x2}, 0}, Digest{0x2})),
+					NewLeaf(&FakePosition{[]byte{0x3}, 0}, Digest{0x3}),
+				),
+			),
+			expectedAuditPath: AuditPath{
+				"00|1": Digest{0x1},
+				"02|0": Digest{0x2},
+			},
+		},
+	}
+
+	visitor := NewAuditPathVisitor(NewComputeHashVisitor(NewFakeXorHasher()))
+
+	for i, c := range testCases {
+		c.visitable.PostOrder(visitor)
+		auditPath := visitor.Result()
+		require.Equalf(t, c.expectedAuditPath, auditPath, "The audit path %v should be equal to the expected %v in test case %d", auditPath, c.expectedAuditPath, i)
+	}
+
+}
