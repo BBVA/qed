4329aabb1840ff32faa0568fc389551ee7246e0b
Alvaro Alda <alvaro.alda@bbva.com>
2018-08-31 13:03:34 +0200

    Add compute hash visitor

--- balloon2/common/cache_test.go
+++ balloon2/common/cache_test.go
@@ -1,12 +1,9 @@
 package common
 
 import (
-	"encoding/binary"
-	"fmt"
 	"testing"
 
 	"github.com/bbva/qed/db"
-	"github.com/bbva/qed/util"
 	"github.com/stretchr/testify/require"
 
 	"github.com/bbva/qed/testutils/storage"
@@ -75,37 +72,4 @@ func TestSimpleCache(t *testing.T) {
 			require.Falsef(t, ok, "The key should not exist in cache in test case %d", i)
 		}
 	}
-
-}
-
-type FakePosition struct {
-	index  []byte
-	height uint16
-}
-
-func (p FakePosition) Index() []byte {
-	return p.index
-}
-
-func (p FakePosition) Height() uint16 {
-	return p.height
-}
-
-func (p FakePosition) Bytes() []byte {
-	b := make([]byte, 34) // Size of the index plus 2 bytes for the height
-	copy(b, p.index)
-	copy(b[len(p.index):], util.Uint16AsBytes(p.height))
-	return b
-}
-
-func (p FakePosition) String() string {
-	return fmt.Sprintf("Pos(%x, %d)", p.index, p.height)
-}
-
-func (p FakePosition) StringId() string {
-	return fmt.Sprintf("%x|%d", p.index, p.height)
-}
-
-func (p FakePosition) IndexAsUint64() uint64 {
-	return binary.BigEndian.Uint64(p.index)
 }
---
+++ balloon2/common/compute.go
@@ -0,0 +1,49 @@
+package common
+
+import "github.com/aalda/trees/log"
+
+type ComputeHashVisitor struct {
+	hasher Hasher
+}
+
+func NewComputeHashVisitor(hasher Hasher) *ComputeHashVisitor {
+	return &ComputeHashVisitor{hasher}
+}
+
+func (v *ComputeHashVisitor) VisitRoot(pos Position, leftResult, rightResult interface{}) interface{} {
+	log.Debugf("Computing root hash in position: %v", pos)
+	return v.interiorHash(pos.Bytes(), leftResult.(Digest), rightResult.(Digest))
+}
+
+func (v *ComputeHashVisitor) VisitNode(pos Position, leftResult, rightResult interface{}) interface{} {
+	log.Debugf("Computing node hash in position: %v", pos)
+	return v.interiorHash(pos.Bytes(), leftResult.(Digest), rightResult.(Digest))
+}
+
+func (v *ComputeHashVisitor) VisitPartialNode(pos Position, leftResult interface{}) interface{} {
+	log.Debugf("Computing partial node hash in position: %v", pos)
+	return v.leafHash(pos.Bytes(), leftResult.(Digest))
+}
+
+func (v *ComputeHashVisitor) VisitLeaf(pos Position, value []byte) interface{} {
+	log.Debugf("Computing leaf hash in position: %v", pos)
+	return v.leafHash(pos.Bytes(), value)
+}
+
+func (v *ComputeHashVisitor) VisitCached(pos Position, cachedDigest Digest) interface{} {
+	log.Debugf("Getting cached hash in position: %v", pos)
+	return cachedDigest
+}
+
+func (v *ComputeHashVisitor) VisitCollectable(pos Position, result interface{}) interface{} {
+	log.Debugf("Getting collectable value in position: %v", pos)
+	return result
+}
+
+func (v *ComputeHashVisitor) leafHash(id, leaf []byte) Digest {
+	return v.hasher.Salted(id, leaf)
+}
+
+func (v *ComputeHashVisitor) interiorHash(id, left, right []byte) Digest {
+	return v.hasher.Salted(id, left, right)
+}
