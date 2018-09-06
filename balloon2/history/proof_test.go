e0ca18021a3bc820d9f2d0dcfd42328251331b10
Alvaro Alda <alvaro.alda@bbva.com>
2018-09-06 10:52:18 +0200

    Add ProveConsistency in history tree

--- balloon2/history/proof_test.go
+++ balloon2/history/proof_test.go
@@ -8,9 +8,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestVerify(t *testing.T) {
+func TestVerifyMembershipProof(t *testing.T) {
 
-	log.SetLogger("TestVerify", log.INFO)
+	log.SetLogger("TestVerifyMembershipProof", log.INFO)
 
 	testCases := []struct {
 		index, version uint64
@@ -152,4 +152,67 @@ func TestVerify(t *testing.T) {
 		assert.Truef(t, correct, "Event should be a member for test case %d", i)
 	}
 
+}
+
+func TestVerifyIncrementalProof(t *testing.T) {
+
+	log.SetLogger("TestVerifyIncrementalProof", log.INFO)
+
+	testCases := []struct {
+		auditPath           common.AuditPath
+		start               uint64
+		end                 uint64
+		expectedStartDigest common.Digest
+		expectedEndDigest   common.Digest
+	}{
+		{
+			auditPath:           common.AuditPath{"0|1": common.Digest{0x1}, "2|0": common.Digest{0x2}, "3|0": common.Digest{0x3}, "4|1": common.Digest{0x1}, "6|0": common.Digest{0x6}},
+			start:               2,
+			end:                 6,
+			expectedStartDigest: common.Digest{0x3},
+			expectedEndDigest:   common.Digest{0x7},
+		},
+		{
+			auditPath:           common.AuditPath{"0|1": common.Digest{0x1}, "2|0": common.Digest{0x2}, "3|0": common.Digest{0x3}, "4|2": common.Digest{0x0}},
+			start:               2,
+			end:                 7,
+			expectedStartDigest: common.Digest{0x3},
+			expectedEndDigest:   common.Digest{0x0},
+		},
+		{
+			auditPath:           common.AuditPath{"0|2": common.Digest{0x0}, "4|0": common.Digest{0x4}, "5|0": common.Digest{0x5}, "6|0": common.Digest{0x6}},
+			start:               4,
+			end:                 6,
+			expectedStartDigest: common.Digest{0x4},
+			expectedEndDigest:   common.Digest{0x7},
+		},
+		{
+			auditPath:           common.AuditPath{"0|2": common.Digest{0x0}, "4|0": common.Digest{0x4}, "5|0": common.Digest{0x5}, "6|1": common.Digest{0x1}},
+			start:               4,
+			end:                 7,
+			expectedStartDigest: common.Digest{0x4},
+			expectedEndDigest:   common.Digest{0x0},
+		},
+		{
+			auditPath:           common.AuditPath{"2|0": common.Digest{0x2}, "3|0": common.Digest{0x3}, "4|0": common.Digest{0x4}, "0|1": common.Digest{0x1}},
+			start:               2,
+			end:                 4,
+			expectedStartDigest: common.Digest{0x3},
+			expectedEndDigest:   common.Digest{0x4},
+		},
+		{
+			auditPath:           common.AuditPath{"0|2": common.Digest{0x0}, "4|1": common.Digest{0x1}, "6|0": common.Digest{0x6}, "7|0": common.Digest{0x7}},
+			start:               6,
+			end:                 7,
+			expectedStartDigest: common.Digest{0x7},
+			expectedEndDigest:   common.Digest{0x0},
+		},
+	}
+
+	hasher := common.NewFakeXorHasher()
+	for _, c := range testCases {
+		proof := NewIncrementalProof(c.start, c.end, c.auditPath, hasher)
+		correct := proof.Verify(c.expectedStartDigest, c.expectedEndDigest)
+		assert.Truef(t, correct, "Events between %d and %d should be consistent", c.start, c.end)
+	}
 }
