f32f783037ce8ee43546c8fe403c19ed2d048a0f
Jose Luis Lucas <joseluis.lucas.simarro@bbva.com>
2018-09-06 17:11:41 +0200

    Hyper: tree+tests and proof tests

--- balloon2/hyper/tree.go
+++ balloon2/hyper/tree.go
@@ -1,11 +1,14 @@
 package hyper
 
 import (
+	"bytes"
 	"math"
 	"sync"
 
 	"github.com/bbva/qed/balloon2/common"
 	"github.com/bbva/qed/db"
+	"github.com/bbva/qed/log"
+	"github.com/bbva/qed/util"
 )
 
 type HyperTree struct {
@@ -36,10 +39,117 @@ func NewHyperTree(hasher common.Hasher, store db.Store, cache common.ModifiableC
 	return tree
 }
 
-func (t *HyperTree) Add(key, value []byte) (common.Digest, []db.Mutation, error) {
-	return nil, nil, nil
+func (t *HyperTree) Add(eventDigest common.Digest, version uint64) (common.Digest, []db.Mutation, error) {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+
+	log.Debugf("Adding event %b with version %d\n", eventDigest, version)
+
+	// visitors
+	computeHash := common.NewComputeHashVisitor(t.hasher)
+	caching := common.NewCachingVisitor(computeHash)
+
+	// build pruning context
+	versionAsBytes := util.Uint64AsBytes(version)
+	context := PruningContext{
+		navigator:     NewHyperTreeNavigator(t.hasher.Len()),
+		cacheResolver: NewSingleTargetedCacheResolver(t.hasher.Len(), t.cacheLevel, eventDigest),
+		cache:         t.cache,
+		store:         t.store,
+		defaultHashes: t.defaultHashes,
+	}
+
+	// traverse from root and generate a visitable pruned tree
+	pruned := NewInsertPruner(eventDigest, versionAsBytes, context).Prune()
+
+	// print := common.NewPrintVisitor(t.hasher.Len())
+	// pruned.PreOrder(print)
+	// log.Debugf("Pruned tree: %s", print.Result())
+
+	// visit the pruned tree
+	rootHash := pruned.PostOrder(caching).(common.Digest)
+
+	// persist mutations
+	cachedElements := caching.Result()
+	mutations := make([]db.Mutation, len(cachedElements))
+	for i, e := range cachedElements {
+		mutations[i] = *db.NewMutation(db.HyperCachePrefix, e.Pos.Bytes(), e.Digest)
+		// update cache
+		t.cache.Put(e.Pos, e.Digest)
+	}
+	// create a mutation for the new leaf
+	leafMutation := db.NewMutation(db.IndexPrefix, eventDigest, versionAsBytes)
+	mutations = append(mutations, *leafMutation)
+
+	log.Debugf("Mutations: %v", mutations)
+
+	return rootHash, mutations, nil
 }
 
-func (t HyperTree) QueryMembership(key []byte) (*QueryProof, error) {
-	return nil, nil
+func (t *HyperTree) QueryMembership(eventDigest common.Digest) (proof *QueryProof, err error) {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+
+	log.Debugf("Getting version for event %b\n", eventDigest)
+
+	pair, err := t.store.Get(db.IndexPrefix, eventDigest) // TODO check existence
+	if err != nil {
+		return nil, err
+	}
+
+	// visitors
+	computeHash := common.NewComputeHashVisitor(t.hasher)
+	calcAuditPath := common.NewAuditPathVisitor(computeHash)
+
+	// build pruning context
+	context := PruningContext{
+		navigator:     NewHyperTreeNavigator(t.hasher.Len()),
+		cacheResolver: NewSingleTargetedCacheResolver(t.hasher.Len(), t.cacheLevel, eventDigest),
+		cache:         t.cache,
+		store:         t.store,
+		defaultHashes: t.defaultHashes,
+	}
+
+	// traverse from root and generate a visitable pruned tree
+	pruned := NewSearchPruner(eventDigest, context).Prune()
+
+	print := common.NewPrintVisitor(t.hasher.Len())
+	pruned.PreOrder(print)
+	log.Debugf("Pruned tree: %s", print.Result())
+
+	// visit the pruned tree
+	pruned.PostOrder(calcAuditPath)
+
+	return NewQueryProof(pair.Key, pair.Value, calcAuditPath.Result(), t.hasher), nil // include version in audit path visitor
+}
+
+func (t *HyperTree) VerifyMembership(proof *QueryProof, version uint64, eventDigest, expectedDigest common.Digest) bool {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+
+	log.Debugf("Verifying membership for eventDigest %x", eventDigest)
+
+	// visitors
+	computeHash := common.NewComputeHashVisitor(t.hasher)
+
+	// build pruning context
+	versionAsBytes := util.Uint64AsBytes(version)
+	context := PruningContext{
+		navigator:     NewHyperTreeNavigator(t.hasher.Len()),
+		cacheResolver: NewSingleTargetedCacheResolver(t.hasher.Len(), t.cacheLevel, eventDigest),
+		cache:         proof.AuditPath,
+		store:         t.store,
+		defaultHashes: t.defaultHashes,
+	}
+
+	// traverse from root and generate a visitable pruned tree
+	pruned := NewVerifyPruner(eventDigest, versionAsBytes, context).Prune()
+
+	print := common.NewPrintVisitor(t.hasher.Len())
+	pruned.PreOrder(print)
+	log.Debugf("Pruned tree: %s", print.Result())
+
+	// visit the pruned tree
+	recomputed := pruned.PostOrder(computeHash).(common.Digest)
+	return bytes.Equal(recomputed, expectedDigest)
 }
