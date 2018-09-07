78621ce4cce62fe660e20f5bfdc4a4bb4359c688
Martin Evgeniev <martin.evgeniev@bbva.com>
2018-09-07 13:03:33 +0200

    TestSearchPruner multiple events
    Former-commit-id: 59c348ba50d74fb8c8b81714eb960f58b2f9c42d [formerly 92af425dc3704b115ed16a029e6b1ad266c0b3d1]
    Former-commit-id: 29c62b1c7712e4e342aa186cde0315250ed0b4bc

--- balloon2/hyper/pruner_test.go
+++ balloon2/hyper/pruner_test.go
@@ -140,19 +140,16 @@ func TestSearchPruner(t *testing.T) {
 	numBits := uint16(8)
 	cacheLevel := uint16(4)
 
-	cache := common.NewSimpleCache(4)
-	// Add element before searching.
-	// BPlus storage (memmory) instead of Badger (disk) for ease.
-	store := bplus.NewBPlusTreeStore()
-	mutations := db.Mutation{db.IndexPrefix, []byte{0}, []byte{0}}
-	store.Mutate(mutations)
-
 	testCases := []struct {
 		key            []byte
+		storeMutation  []db.Mutation
 		expectedPruned common.Visitable
 	}{
 		{
 			key: []byte{0},
+			storeMutation: []db.Mutation{
+				*db.NewMutation(db.IndexPrefix, []byte{0}, []byte{0}),
+			},
 			expectedPruned: common.NewRoot(NewPosition([]byte{0}, 8),
 				common.NewNode(NewPosition([]byte{0}, 7),
 					common.NewNode(NewPosition([]byte{0}, 6),
@@ -180,9 +177,50 @@ func TestSearchPruner(t *testing.T) {
 					common.NewCached(NewPosition([]byte{128}, 7), common.Digest{0})),
 			),
 		},
+		{
+			key: []byte{6},
+			storeMutation: []db.Mutation{
+				*db.NewMutation(db.IndexPrefix, []byte{1}, []byte{1}),
+				*db.NewMutation(db.IndexPrefix, []byte{6}, []byte{6}),
+			},
+			expectedPruned: common.NewRoot(NewPosition([]byte{0}, 8),
+				common.NewNode(NewPosition([]byte{0}, 7),
+					common.NewNode(NewPosition([]byte{0}, 6),
+						common.NewNode(NewPosition([]byte{0}, 5),
+							common.NewNode(NewPosition([]byte{0}, 4),
+								common.NewNode(NewPosition([]byte{0}, 3),
+									common.NewCollectable(NewPosition([]byte{0}, 2),
+										common.NewNode(NewPosition([]byte{0}, 2),
+											common.NewNode(NewPosition([]byte{0}, 1),
+												common.NewCached(NewPosition([]byte{0}, 0), common.Digest{0}),
+												common.NewLeaf(NewPosition([]byte{1}, 0), []byte{1})),
+											common.NewCached(NewPosition([]byte{2}, 1), common.Digest{0}))),
+									common.NewNode(NewPosition([]byte{4}, 2),
+										common.NewCollectable(NewPosition([]byte{4}, 1),
+											common.NewCached(NewPosition([]byte{4}, 1), common.Digest{0})),
+										common.NewNode(NewPosition([]byte{6}, 1),
+											common.NewLeaf(NewPosition([]byte{6}, 0), []byte{6}),
+											common.NewCollectable(NewPosition([]byte{7}, 0),
+												common.NewCached(NewPosition([]byte{7}, 0), common.Digest{0}))))),
+								common.NewCollectable(NewPosition([]byte{8}, 3),
+									common.NewCached(NewPosition([]byte{8}, 3), common.Digest{0}))),
+							common.NewCollectable(NewPosition([]byte{16}, 4),
+								common.NewCached(NewPosition([]byte{16}, 4), common.Digest{0}))),
+						common.NewCollectable(NewPosition([]byte{32}, 5),
+							common.NewCached(NewPosition([]byte{32}, 5), common.Digest{0}))),
+					common.NewCollectable(NewPosition([]byte{64}, 6),
+						common.NewCached(NewPosition([]byte{64}, 6), common.Digest{0}))),
+				common.NewCollectable(NewPosition([]byte{128}, 7),
+					common.NewCached(NewPosition([]byte{128}, 7), common.Digest{0}))),
+		},
 	}
 
 	for i, c := range testCases {
+		store := bplus.NewBPlusTreeStore()
+		store.Mutate(c.storeMutation...)
+
+		cache := common.NewSimpleCache(4)
+
 		context := PruningContext{
 			navigator:     NewHyperTreeNavigator(numBits),
 			cacheResolver: NewSingleTargetedCacheResolver(numBits, cacheLevel, c.key),
@@ -198,7 +236,6 @@ func TestSearchPruner(t *testing.T) {
 		assert.Equalf(t, c.expectedPruned, pruned, "The pruned trees should match for test case %d", i)
 	}
 }
-
 func TestVerifyPruner(t *testing.T) {
 
 	numBits := uint16(8)
