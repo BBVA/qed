e232c45df8b6d6344c041663673d20ca4cf2992c
pancho horrillo <pedrofelipe.horrillo@bbva.com>
2018-09-20 11:16:54 +0200

    Provide a join mechanism to enable raft clustering
    Co-authored-by: iknite <enrique@iknite.com>
    
    Former-commit-id: 44e2849c75f71240b3ba5f13c1dbde51227bd290

---
+++ api/mgmthttp/mgmthttp.go
@@ -0,0 +1,67 @@
+/*
+   Copyright 2018 Banco Bilbao Vizcaya Argentaria, S.A.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package mgmthttp
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/bbva/qed/balloon"
+)
+
+// NewMgmtHttp will return a mux server with the endpoint required to
+// tamper the server. it's a internal debug implementation. Running a server
+// with this enabled will run useless the qed server.
+func NewMgmtHttp(raftBalloon balloon.RaftBalloonApi) *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/join", joinHandle(raftBalloon))
+	return mux
+}
+
+func joinHandle(raftBalloon balloon.RaftBalloonApi) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := map[string]string{}
+
+		if err := json.NewDecoder(r.Body).Decode(&m); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		if len(m) != 2 {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		remoteAddr, ok := m["addr"]
+		if !ok {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		nodeID, ok := m["id"]
+		if !ok {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		if err := raftBalloon.Join(nodeID, remoteAddr); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+	}
+}
