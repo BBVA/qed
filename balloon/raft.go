f7d50e44901cd8cddcc85d552171af9d06ed35fc
Gabriel Díaz <g.diaz.lopezllave@bbva.com>
2018-09-21 16:27:31 +0200

    Modify raft structure to facilitate testing and add adminsitration functionality

--- balloon/raft.go
+++ balloon/raft.go
@@ -61,34 +61,43 @@ type RaftBalloonApi interface {
 
 // RaftBalloon is a replicated verifiable key-value store, where changes are made via Raft consensus.
 type RaftBalloon struct {
-	raftDir      string
-	raftBindAddr string
-	raftID       string     // Node ID.
-	raft         *raft.Raft // The consensus mechanism.
+	path string // Base path for the node
+	addr string // Node addr
+	id   string // Node ID
 
-	lock     sync.RWMutex
-	closedMu sync.Mutex
-	closed   bool // Has the RaftBalloon been closed?
+	raft struct {
+		api          *raft.Raft             // The consensus mechanism
+		transport    *raft.NetworkTransport // Raft network transport
+		config       *raft.Config           //Config provides any necessary configuration for the Raft server.
+		nodes        *raft.Configuration    //Configuration tracks which servers are in the cluster, and whether they have votes.
+		applyTimeout time.Duration
+	}
 
-	wg   sync.WaitGroup
-	done chan struct{}
+	store struct {
+		db        storage.ManagedStore    // Persistent database
+		log       *raftbadger.BadgerStore // Persistent log store
+		stable    *raftbadger.BadgerStore // Persistent k-v store
+		snapshots *raft.FileSnapshotStore // Persisten snapstop store
+	}
 
-	store       storage.ManagedStore    // Persistent database
-	logStore    *raftbadger.BadgerStore // Persistent log store.
-	stableStore *raftbadger.BadgerStore // Persistent k-v store.
+	sync.Mutex
+	closed bool
+	wg     sync.WaitGroup
+	done   chan struct{}
 
 	fsm *BalloonFSM // balloon's finite state machine
+
 }
 
 // New returns a new RaftBalloon.
-func NewRaftBalloon(raftDir, raftBindAddr, raftID string, store storage.ManagedStore) (*RaftBalloon, error) {
+func NewRaftBalloon(path, addr, id string, store storage.ManagedStore) (*RaftBalloon, error) {
 
 	// Create the log store and stable store
-	logStore, err := raftbadger.NewBadgerStore(raftDir + "/logs")
+	logStore, err := raftbadger.NewBadgerStore(path + "/logs")
 	if err != nil {
 		return nil, fmt.Errorf("new badger store: %s", err)
 	}
-	stableStore, err := raftbadger.NewBadgerStore(raftDir + "/config")
+	stableStore, err := raftbadger.NewBadgerStore(path + "/config")
 	if err != nil {
 		return nil, fmt.Errorf("new badger store: %s", err)
 	}
@@ -99,74 +108,74 @@ func NewRaftBalloon(raftDir, raftBindAddr, raftID string, store storage.ManagedS
 		return nil, fmt.Errorf("new balloon fsm: %s", err)
 	}
 
-	return &RaftBalloon{
-		raftDir:      raftDir,
-		raftBindAddr: raftBindAddr,
-		raftID:       raftID,
-		done:         make(chan struct{}),
-		store:        store,
-		logStore:     logStore,
-		stableStore:  stableStore,
-		fsm:          fsm,
-	}, nil
+	rb := &RaftBalloon{
+		path: path,
+		addr: addr,
+		id:   id,
+		done: make(chan struct{}),
+		fsm:  fsm,
+	}
+
+	rb.store.db = store
+	rb.store.log = logStore
+	rb.store.stable = stableStore
+
+	return rb, nil
 }
 
 // Open opens the Balloon. If no joinAddr is provided, then there are no existing peers,
 // then this node becomes the first node, and therefore, leader of the cluster.
-// Otherwise, it will try to join the cluster via the aforementioned joinAddr.
-func (b *RaftBalloon) Open(joinAddr, raftAddr, nodeID string) error {
+func (b *RaftBalloon) Open(bootstrap bool) error {
+	b.Lock()
+	defer b.Unlock()
 
-	b.closedMu.Lock()
-	defer b.closedMu.Unlock()
 	if b.closed {
 		return ErrBalloonInvalidState
 	}
 
-	log.Infof("opening balloon with node ID %s", b.raftID)
+	log.Infof("opening balloon with node ID %s", b.id)
 
 	// Setup Raft configuration
-	config := raft.DefaultConfig()
-	config.LocalID = raft.ServerID(b.raftID)
+	b.raft.config = raft.DefaultConfig()
+	b.raft.config.LocalID = raft.ServerID(b.id)
+	b.raft.applyTimeout = 10 * time.Second
 
 	// Setup Raft communication
-	addr, err := net.ResolveTCPAddr("tcp", b.raftBindAddr)
+	raddr, err := net.ResolveTCPAddr("tcp", b.addr)
 	if err != nil {
 		return err
 	}
-	transport, err := raft.NewTCPTransport(b.raftBindAddr, addr, 3, 10*time.Second, os.Stderr)
+
+	b.raft.transport, err = raft.NewTCPTransport(b.addr, raddr, 3, 10*time.Second, os.Stderr)
 	if err != nil {
 		return err
 	}
 
 	// Create the snapshot store. This allows the Raft to truncate the log.
-	snapshots, err := raft.NewFileSnapshotStore(b.raftDir, retainSnapshotCount, os.Stderr)
+	b.store.snapshots, err = raft.NewFileSnapshotStore(b.path+"/snapshots", retainSnapshotCount, os.Stderr)
 	if err != nil {
 		return fmt.Errorf("file snapshot store: %s", err)
 	}
 
 	// Instantiate the Raft system
-	b.raft, err = raft.NewRaft(config, b.fsm, b.logStore, b.stableStore, snapshots, transport)
+	b.raft.api, err = raft.NewRaft(b.raft.config, b.fsm, b.store.log, b.store.stable, b.store.snapshots, b.raft.transport)
 	if err != nil {
 		return fmt.Errorf("new raft: %s", err)
 	}
 
-	if joinAddr == "" {
+	if bootstrap {
 		log.Info("bootstrap needed")
-		configuration := raft.Configuration{
+		b.raft.nodes = &raft.Configuration{
 			Servers: []raft.Server{
 				{
-					ID:      config.LocalID,
-					Address: transport.LocalAddr(),
+					ID:      b.raft.config.LocalID,
+					Address: b.raft.transport.LocalAddr(),
 				},
 			},
 		}
-		b.raft.BootstrapCluster(configuration)
+		b.raft.api.BootstrapCluster(*b.raft.nodes)
 	} else {
 		log.Info("no bootstrap needed")
-		// If join was specified, make the join request.
-		if err := join(joinAddr, raftAddr, nodeID); err != nil {
-			log.Fatalf("failed to join node at %s: %s", joinAddr, err.Error())
-		}
 	}
 
 	return nil
@@ -186,15 +195,16 @@ func join(joinAddr, raftAddr, nodeID string) error {
 	return nil
 }
 
-// Join joins a node, identified by id and located ad addr, to this store.
+// Join joins a node, identified by id and located at addr, to this store.
 // The node must be ready to respond to Raft communications at that address.
+// This must be called from the Leader or it will fail.
 func (b *RaftBalloon) Join(nodeID, addr string) error {
 
 	log.Infof("received join request for remote node %s at %s", nodeID, addr)
 
-	configFuture := b.raft.GetConfiguration()
+	configFuture := b.raft.api.GetConfiguration()
 	if err := configFuture.Error(); err != nil {
-		log.Errorf("failed to get raft configuration: %v", err)
+		log.Errorf("failed to get raft servers configuration: %v", err)
 		return err
 	}
 
@@ -209,14 +219,14 @@ func (b *RaftBalloon) Join(nodeID, addr string) error {
 				return nil
 			}
 
-			future := b.raft.RemoveServer(srv.ID, 0, 0)
+			future := b.raft.api.RemoveServer(srv.ID, 0, 0)
 			if err := future.Error(); err != nil {
 				return fmt.Errorf("error removing existing node %s at %s: %s", nodeID, addr, err)
 			}
 		}
 	}
 
-	f := b.raft.AddVoter(raft.ServerID(nodeID), raft.ServerAddress(addr), 0, 0)
+	f := b.raft.api.AddVoter(raft.ServerID(nodeID), raft.ServerAddress(addr), 0, 0)
 	if e := f.(raft.Future); e.Error() != nil {
 		if e.Error() == raft.ErrNotLeader {
 			return ErrNotLeader
@@ -231,8 +241,8 @@ func (b *RaftBalloon) Join(nodeID, addr string) error {
 // Close closes the RaftBalloon. If wait is true, waits for a graceful shutdown.
 // Once closed, a RaftBalloon may not be re-opened.
 func (b *RaftBalloon) Close(wait bool) error {
-	b.closedMu.Lock()
-	defer b.closedMu.Unlock()
+	b.Lock()
+	defer b.Unlock()
 	if b.closed {
 		return nil
 	}
@@ -244,38 +254,155 @@ func (b *RaftBalloon) Close(wait bool) error {
 	b.wg.Wait()
 
 	// close database
-	if err := b.store.Close(); err != nil {
+	if err := b.store.db.Close(); err != nil {
 		return err
 	}
-	b.store = nil
+	b.store.db = nil
 
 	// shutdown raft
-	if b.raft != nil {
-		f := b.raft.Shutdown()
+	if b.raft.api != nil {
+		f := b.raft.api.Shutdown()
 		if wait {
 			if e := f.(raft.Future); e.Error() != nil {
 				return e.Error()
 			}
 		}
-		b.raft = nil
+		b.raft.api = nil
 	}
 
 	// close raft store
-	if err := b.logStore.Close(); err != nil {
+	if err := b.store.log.Close(); err != nil {
 		return err
 	}
-	if err := b.stableStore.Close(); err != nil {
+	if err := b.store.stable.Close(); err != nil {
 		return err
 	}
-	b.logStore = nil
-	b.stableStore = nil
+	b.store.log = nil
+	b.store.stable = nil
 
 	return nil
 }
 
+// Wait until node becomes leader or time is out
+func (b RaftBalloon) WaitForLeader(timeout time.Duration) (string, error) {
+	tck := time.NewTicker(leaderWaitDelay)
+	defer tck.Stop()
+	tmr := time.NewTimer(timeout)
+	defer tmr.Stop()
+
+	for {
+		select {
+		case <-tck.C:
+			l := string(b.raft.api.Leader())
+			if l != "" {
+				return l, nil
+			}
+		case <-tmr.C:
+			return "", fmt.Errorf("timeout expired")
+		}
+	}
+}
+
+func (b RaftBalloon) IsLeader() bool {
+	return b.raft.api.State() == raft.Leader
+}
+
+// Addr returns the address of the store.
+func (b RaftBalloon) Addr() string {
+	return string(b.raft.transport.LocalAddr())
+}
+
+// LeaderAddr returns the Raft address of the current leader. Returns a
+// blank string if there is no leader.
+func (b RaftBalloon) LeaderAddr() string {
+	return string(b.raft.api.Leader())
+}
+
+// ID returns the Raft ID of the store.
+func (b RaftBalloon) ID() string {
+	return b.id
+}
+
+// LeaderID returns the node ID of the Raft leader. Returns a
+// blank string if there is no leader, or an error.
+func (b RaftBalloon) LeaderID() (string, error) {
+	addr := b.LeaderAddr()
+	configFuture := b.raft.api.GetConfiguration()
+	if err := configFuture.Error(); err != nil {
+		log.Infof("failed to get raft configuration: %v", err)
+		return "", err
+	}
+
+	for _, srv := range configFuture.Configuration().Servers {
+		if srv.Address == raft.ServerAddress(addr) {
+			return string(srv.ID), nil
+		}
+	}
+	return "", nil
+}
+
+// Nodes returns the slice of nodes in the cluster, sorted by ID ascending.
+func (b RaftBalloon) Nodes() ([]raft.Server, error) {
+	f := b.raft.api.GetConfiguration()
+	if f.Error() != nil {
+		return nil, f.Error()
+	}
+
+	return f.Configuration().Servers, nil
+
+}
+
+// Remove removes a node from the store, specified by ID.
+func (b *RaftBalloon) Remove(id string) error {
+	log.Infof("received request to remove node %s", id)
+	if err := b.remove(id); err != nil {
+		log.Infof("failed to remove node %s: %s", id, err.Error())
+		return err
+	}
+
+	log.Infof("node %s removed successfully", id)
+	return nil
+}
+
+// remove removes the node, with the given ID, from the cluster.
+func (b *RaftBalloon) remove(id string) error {
+	if b.raft.api.State() != raft.Leader {
+		return ErrNotLeader
+	}
+
+	f := b.raft.api.RemoveServer(raft.ServerID(id), 0, 0)
+	if f.Error() != nil {
+		if f.Error() == raft.ErrNotLeader {
+			return ErrNotLeader
+		}
+		return f.Error()
+	}
+
+	// TODO: implement metadataDelete
+	c, err := newCommand(metadataDelete, id)
+	cb, err := json.Marshal(c)
+	if err != nil {
+		return err
+	}
+	f = b.raft.api.Apply(cb, b.raft.applyTimeout)
+	if e := f.(raft.Future); e.Error() != nil {
+		if e.Error() == raft.ErrNotLeader {
+			return ErrNotLeader
+		}
+		e.Error()
+	}
+
+	return nil
+}
+
+/*
+	RaftBalloon API implemnts the Ballon API in the RAFT system
+
+*/
+
 func (b *RaftBalloon) Add(event []byte) (*Commitment, error) {
-	b.lock.Lock()
-	defer b.lock.Unlock()
+	b.Lock()
+	defer b.Unlock()
 
 	cmd, err := newCommand(insert, newInsertSubCommand(event, b.fsm.Version()))
 	if err != nil {
@@ -286,7 +413,7 @@ func (b *RaftBalloon) Add(event []byte) (*Commitment, error) {
 		return nil, err
 	}
 
-	f := b.raft.Apply(cmdBytes, raftTimeout)
+	f := b.raft.api.Apply(cmdBytes, raftTimeout)
 	if e := f.(raft.Future); e.Error() != nil {
 		if e.Error() == raft.ErrNotLeader {
 			return nil, ErrNotLeader
@@ -303,27 +430,4 @@ func (b RaftBalloon) QueryMembership(event []byte, version uint64) (*MembershipP
 
 func (b RaftBalloon) QueryConsistency(start, end uint64) (*IncrementalProof, error) {
 	return b.fsm.QueryConsistency(start, end)
-}
-
-func (b RaftBalloon) WaitForLeader(timeout time.Duration) (string, error) {
-	tck := time.NewTicker(leaderWaitDelay)
-	defer tck.Stop()
-	tmr := time.NewTimer(timeout)
-	defer tmr.Stop()
-
-	for {
-		select {
-		case <-tck.C:
-			l := string(b.raft.Leader())
-			if l != "" {
-				return l, nil
-			}
-		case <-tmr.C:
-			return "", fmt.Errorf("timeout expired")
-		}
-	}
-}
-
-func (b RaftBalloon) IsLeader() bool {
-	return b.raft.State() == raft.Leader
 }
