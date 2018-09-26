0261be3b12c1399072e986d13254d1ab48a3b339
Alvaro Alda <alvaro.alda@bbva.com>
2018-09-26 17:06:27 +0200

    Replace json marshaling with message pack for raft commands
    Former-commit-id: 531a5cc8cc46517b0d02dc6a5910982b4bac37ea [formerly 1f24b77276e190933ea73d19be50bed4f8a4c14f]
    Former-commit-id: 631b2bbe445ce6462305a0fb605d1c00353c126e

---
+++ balloon/commands/command.go
@@ -0,0 +1,56 @@
+/*
+   Copyright 2018 Banco Bilbao Vizcaya Argentaria, S.A.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package commands
+
+import (
+	"bytes"
+
+	"github.com/hashicorp/go-msgpack/codec"
+)
+
+// CommandType are commands that affect the state of the cluster,
+// and must go through raft.
+type CommandType uint8
+
+const (
+	AddEventCommandType       CommandType = 0 // Commands which modify the database.
+	MetadataDeleteCommandType CommandType = 1
+)
+
+type AddEventCommand struct {
+	Event []byte
+}
+
+type MetadataDeleteCommand struct {
+	Id string
+}
+
+// msgpackHandle is a shared handle for encoding/decoding of structs
+var msgpackHandle = &codec.MsgpackHandle{}
+
+// Decode is used to encode a MsgPack object with type prefix.
+func Decode(buf []byte, out interface{}) error {
+	return codec.NewDecoder(bytes.NewReader(buf), msgpackHandle).Decode(out)
+}
+
+// Encode is used to encode a MsgPack object with type prefix
+func Encode(t CommandType, cmd interface{}) ([]byte, error) {
+	var buf bytes.Buffer
+	buf.WriteByte(uint8(t))
+	err := codec.NewEncoder(&buf, msgpackHandle).Encode(cmd)
+	return buf.Bytes(), err
+}
