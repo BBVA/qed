70960652d83bf4d1c7fabb0f0251eb141fd74e87
iknite <enrique@iknite.com>
2018-09-26 13:31:23 +0200

    Refactor promoted fields on visitors

--- balloon/common/audit.go
+++ balloon/common/audit.go
@@ -49,50 +49,21 @@ func (f FakeVerifiable) AuditPath() AuditPath {
 }
 
 type AuditPathVisitor struct {
-	decorated *ComputeHashVisitor
 	auditPath AuditPath
+
+	*ComputeHashVisitor
 }
 
 func NewAuditPathVisitor(decorated *ComputeHashVisitor) *AuditPathVisitor {
-	return &AuditPathVisitor{decorated, make(AuditPath)}
+	return &AuditPathVisitor{ComputeHashVisitor: decorated, auditPath: make(AuditPath)}
 }
 
 func (v AuditPathVisitor) Result() AuditPath {
 	return v.auditPath
 }
 
-func (v *AuditPathVisitor) VisitRoot(pos Position, leftResult, rightResult interface{}) interface{} {
-	// by-pass
-	return v.decorated.VisitRoot(pos, leftResult, rightResult)
-}
-
-func (v *AuditPathVisitor) VisitNode(pos Position, leftResult, rightResult interface{}) interface{} {
-	// by-pass
-	return v.decorated.VisitNode(pos, leftResult, rightResult)
-}
-
-func (v *AuditPathVisitor) VisitPartialNode(pos Position, leftResult interface{}) interface{} {
-	// by-pass
-	return v.decorated.VisitPartialNode(pos, leftResult)
-}
-
-func (v *AuditPathVisitor) VisitLeaf(pos Position, eventDigest []byte) interface{} {
-	// ignore. target leafs not included in path
-	return v.decorated.VisitLeaf(pos, eventDigest)
-}
-
-func (v *AuditPathVisitor) VisitCached(pos Position, cachedDigest hashing.Digest) interface{} {
-	// by-pass
-	return v.decorated.VisitCached(pos, cachedDigest)
-}
-
 func (v *AuditPathVisitor) VisitCollectable(pos Position, result interface{}) interface{} {
-	digest := v.decorated.VisitCollectable(pos, result).(hashing.Digest)
+	digest := v.ComputeHashVisitor.VisitCollectable(pos, result).(hashing.Digest)
 	v.auditPath[pos.StringId()] = digest
 	return digest
-}
-
-func (v *AuditPathVisitor) VisitCacheable(pos Position, result interface{}) interface{} {
-	// by-pass
-	return v.decorated.VisitCacheable(pos, result)
 }
