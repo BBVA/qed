1351a49a0064fec2974416b5be5f73c6efdc7ad7
iknite <enrique@iknite.com>
2018-10-01 12:56:02 +0200

    Separate Exists outside struct in hyper.QueryMembership
    Former-commit-id: b3e7a3963e68bdde49df13b000c8caeb95749bea [formerly eb7000498b1f21b1223f5ffd4d2e7da3fda2b995]
    Former-commit-id: be2499ae249c589492327b2fd1fe1895938721b3

--- balloon/hyper/tree.go
+++ balloon/hyper/tree.go
@@ -99,15 +99,15 @@ func (t *HyperTree) Add(eventDigest hashing.Digest, version uint64) (hashing.Dig
 	return rootHash, mutations, nil
 }
 
-func (t *HyperTree) QueryMembership(eventDigest hashing.Digest) (proof *QueryProof, err error) {
+func (t *HyperTree) QueryMembership(eventDigest hashing.Digest) (proof *QueryProof, exists bool, err error) {
 	t.lock.Lock()
 	defer t.lock.Unlock()
 
 	pair, err := t.store.Get(storage.IndexPrefix, eventDigest)
 	if err == storage.ErrKeyNotFound {
-		return &QueryProof{}, nil
+		return nil, false, nil
 	} else if err != nil {
-		return nil, err
+		return nil, false, err
 	}
 
 	// visitors
@@ -129,7 +129,7 @@ func (t *HyperTree) QueryMembership(eventDigest hashing.Digest) (proof *QueryPro
 	// visit the pruned tree
 	pruned.PostOrder(calcAuditPath)
 
-	return NewQueryProof(pair.Key, pair.Value, calcAuditPath.Result(), t.hasherF()), nil // TODO include version in audit path visitor
+	return NewQueryProof(pair.Key, pair.Value, calcAuditPath.Result(), t.hasherF()), true, nil // TODO include version in audit path visitor
 }
 
 func (t *HyperTree) VerifyMembership(proof *QueryProof, version uint64, eventDigest, expectedDigest hashing.Digest) bool {
