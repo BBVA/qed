a125997e1fa19b8293cab350b30155bdcb4ce0de
Martin Evgeniev <martin.evgeniev@bbva.com>
2018-10-11 13:08:07 +0200

    Update vegeta attack and go modules
    Due to issues with go modules with many editors VS-Code, Spacemacs, etc... We've decided to still use a classic GOPATH
    for local developement. In order to achieve that we've updated our code to support latest versions of external
    dependencies.
    
    Co-authored-by: pancho horrilo <pedrofelipe.horrillo@bbva.com>
    
    Former-commit-id: c965632363ea87c3d85030034619fa4739a8c5a5

--- tests/attack_add/attack_add.go
+++ tests/attack_add/attack_add.go
@@ -36,13 +36,14 @@ func main() {
 	duration := flag.Duration("d", 10*time.Second, "Duration")
 	endpoint := flag.String("e", "http://localhost:8080/events", "Endpoint")
 	apikey := flag.String("k", "apikey", "apikey")
-	rate := flag.Uint64("r", 100, "Request per second")
+	rate := flag.Int("r", 100, "Request per second")
 	flag.Parse()
 
 	targeter := myTargeter(*endpoint, http.Header{"Api-Key": []string{*apikey}})
 
 	atk := vegeta.NewAttacker(vegeta.Connections(*conn), vegeta.Workers(*workers), vegeta.Timeout(*timeout))
-	res := atk.Attack(targeter, *rate, *duration, "attack")
+	vgrate := vegeta.Rate{Freq: *rate, Per: time.Second}
+	res := atk.Attack(targeter, vgrate, *duration, "attack")
 	enc := vegeta.NewEncoder(os.Stdout)
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, os.Interrupt)
