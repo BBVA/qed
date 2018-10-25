e18e1e49f0ba58bd83157caf481efd030c8bbaa5
Gabriel Díaz <g.diaz.lopezllave@bbva.com>
2018-10-25 12:51:54 +0200

    Use BigEndian encoding as its whats badger use, so store.GetLastVersion works.
    Former-commit-id: 9ba3a33249f10054893ce7e8f3c594d05619268c [formerly d5306197a45bc904332d81f320963d6c0228b7df]
    Former-commit-id: 7e1ffa78f1315492ed291b5a4dd800054c08a9a2

--- util/bytes.go
+++ util/bytes.go
@@ -20,20 +20,16 @@ import "encoding/binary"
 
 func Uint64AsBytes(i uint64) []byte {
 	b := make([]byte, 8)
-	binary.LittleEndian.PutUint64(b, i)
+	binary.BigEndian.PutUint64(b, i)
 	return b
 }
 
 func Uint16AsBytes(i uint16) []byte {
 	b := make([]byte, 2)
-	binary.LittleEndian.PutUint16(b, i)
+	binary.BigEndian.PutUint16(b, i)
 	return b
 }
 
 func BytesAsUint64(b []byte) uint64 {
-	var out uint64
-	for i, x := range b {
-		out |= uint64(x) << uint64(i*8)
-	}
-	return out
+	return binary.BigEndian.Uint64(b)
 }
