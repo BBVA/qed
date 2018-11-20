1cfe5995e9f23e076581229e59fa11e4e587c7a0
Alvaro Alda <alvaro.alda@bbva.com>
2018-11-20 11:29:38 +0100

    Add monitor skeleton
    Former-commit-id: 3c92312ca74198104369af0b985dbb93e7b315dd [formerly 77497110c6ddc090c996dc353baed120ef28148f]
    Former-commit-id: 1392575c7b4d8bd3e1ca91fc333806de1f1aa2a3

--- tests/gossip/run_gossip.sh
+++ tests/gossip/run_gossip.sh
@@ -19,7 +19,13 @@ sleep 1s
 for i in `seq 1 $1`;
 do
 	xterm -hold -e "go run $GOPATH/src/github.com/bbva/qed/main.go agent -k key -l debug --bind 127.0.0.1:910$i --join $master --node auditor$i --role auditor" &
-	pids[${i}]=$!
+	pids+=($!)
+done 
+
+for i in `seq 1 $2`;
+do
+	xterm -hold -e "go run $GOPATH/src/github.com/bbva/qed/main.go agent -k key -l debug --bind 127.0.0.1:920$i --join $master --node monitor$i --role monitor" &
+	pids+=($!)
 done 
 
 for pid in ${pids[*]}; do
