957220e54cd1a73f4b904576fd40a77e387173b3
Gabriel Díaz <g.diaz.lopezllave@bbva.com>
2018-11-27 17:55:50 +0100

    Fix routing and processor.

--- tests/gossip/run_gossip.sh
+++ tests/gossip/run_gossip.sh
@@ -13,19 +13,19 @@
 
 master="127.0.0.1:9100"
 qed="127.0.0.1:8080"
-go run $GOPATH/src/github.com/bbva/qed/main.go start  -k key -l debug  --node-id server0 --gossip-addr $master --raft-addr 127.0.0.1:9000 -y $HOME/.ssh/id_ed25519 &
+go run $GOPATH/src/github.com/bbva/qed/main.go start  -k key -l silent  --node-id server0 --gossip-addr $master --raft-addr 127.0.0.1:9000 -y $HOME/.ssh/id_ed25519 &
 pids[0]=$!
 sleep 1s
 
 for i in `seq 1 $1`;
 do
-	xterm -hold -e "go run $GOPATH/src/github.com/bbva/qed/main.go agent auditor -k key -l debug --bind 127.0.0.1:910$i --join $master --endpoints $qed --node auditor$i" &
+	xterm -hold -e "go run $GOPATH/src/github.com/bbva/qed/main.go agent auditor -k key -l silent --bind 127.0.0.1:910$i --join $master --endpoints $qed --node auditor$i" &
 	pids+=($!)
 done 
 
 for i in `seq 1 $2`;
 do
-	xterm -hold -e "go run $GOPATH/src/github.com/bbva/qed/main.go agent monitor -k key -l debug --bind 127.0.0.1:920$i --join $master --endpoints $qed --node monitor$i" &
+	xterm -hold -e "go run $GOPATH/src/github.com/bbva/qed/main.go agent monitor -k key -l silent --bind 127.0.0.1:920$i --join $master --endpoints $qed --node monitor$i" &
 	pids+=($!)
 done 
 
