f853921cc1a2e6928e1d8ea94917520d759206ca
Gabriel Díaz López de la Llave <gdiazlo@localhost.localdomain>
2018-11-29 19:45:06 +0100

    wip: auditor
    Former-commit-id: ec93f06c63c11c475899bee3ab4f8d82d354a958 [formerly 1182af3adb549f71b0d59013455efabe35f8bb28]
    Former-commit-id: 68338e175ac623a1be319a573d7a8e7459f467d0

--- gossip/auditor/auditor.go
+++ gossip/auditor/auditor.go
@@ -13,16 +13,145 @@
 
 package auditor
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/bbva/qed/client"
+	"github.com/bbva/qed/hashing"
+	"github.com/bbva/qed/log"
+	"github.com/bbva/qed/protocol"
+)
+
 type Config struct {
+	QEDEndpoints          []string
+	APIKey                string
+	TaskExecutionInterval time.Duration
+	MaxInFlightTasks      int
 }
 
 func DefaultConfig() *Config {
-	return nil
+	return &Config{
+		TaskExecutionInterval: 200 * time.Millisecond,
+		MaxInFlightTasks:      10,
+	}
 }
 
 type Auditor struct {
+	client *client.HttpClient
+	conf   *Config
+
+	taskCh          chan *QueryTask
+	quitCh          chan bool
+	executionTicker *time.Ticker
 }
 
-func NewAuditor(conf *Config) *Auditor {
-	return &Auditor{}
+func NewAuditor(conf *Config) (*Auditor, error) {
+
+	client := client.NewHttpClient(conf.QEDEndpoints[0], conf.APIKey)
+
+	auditor := &Auditor{
+		client: client,
+		conf:   conf,
+		taskCh: make(chan *QueryTask, 100),
+		quitCh: make(chan bool),
+	}
+
+	go auditor.runTaskDispatcher()
+
+	return auditor, nil
+}
+
+type QuerTask interface {
+	Do(*protocol.BatchSnapshots)
+}
+
+type IncrementalTask struct {
+	client                     *client.HttpClient
+	Start, End                 uint64
+	StartSnapshot, EndSnapshot *protocol.Snapshot
+}
+
+func (t *IncrementalTask) Do() {
+	log.Debug("Executing task: %+v", t)
+	fmt.Printf("Executing task: %+v\n", t)
+	resp, err := t.client.Incremental(t.Start, t.End)
+	if err != nil {
+		// retry
+		log.Errorf("Error executing incremental query: %v", err)
+	}
+	ok := t.client.VerifyIncremental(resp, t.StartSnapshot, t.EndSnapshot, hashing.NewSha256Hasher())
+	fmt.Printf("Consistency between versions %d and %d: %v\n", t.Start, t.End, ok)
+}
+
+type MembershipTask struct {
+	client *client.HttpClient
+	S      *protocol.SignedSnapshot
+}
+
+func (t *MembershipTask) Do() {
+	log.Debug("Executing task: %+v", t)
+	fmt.Printf("Executing task: %+v\n", t)
+	resp, err := t.client.Membership(t.S.Snapshot.EventDigest, t.S.Snapshot.Version)
+	if err != nil {
+		// retry
+		log.Errorf("Error executing incremental query: %v", err)
+	}
+	ok := t.client.Verify(resp, t.StartSnapshot, t.S.Snapshot., hashing.NewSha256Hasher())
+	fmt.Printf("Membership\n", t.Start, t.End, ok)
+}
+
+func (m Auditor) Process(b *protocol.BatchSnapshots) {
+
+	first := b.Snapshots[0].Snapshot
+	last := b.Snapshots[len(b.Snapshots)-1].Snapshot
+
+	log.Debugf("Processing batch from versions %d to %d", first.Version, last.Version)
+
+	task := &QueryTask{
+		Start:         first.Version,
+		End:           last.Version,
+		StartSnapshot: first,
+		EndSnapshot:   last,
+	}
+
+	m.taskCh <- task
+}
+
+func (m *Auditor) runTaskDispatcher() {
+	m.executionTicker = time.NewTicker(m.conf.TaskExecutionInterval)
+	for {
+		select {
+		case <-m.executionTicker.C:
+			log.Debug("Dispatching tasks...")
+			m.dispatchTasks()
+		case <-m.quitCh:
+			return
+		}
+	}
+}
+
+func (m *Auditor) Shutdown() {
+	m.executionTicker.Stop()
+	m.quitCh <- true
+	close(m.quitCh)
+	close(m.taskCh)
+}
+
+func (m *Auditor) dispatchTasks() {
+	count := 0
+	var task *QueryTask
+	defer log.Debugf("%d tasks dispatched", count)
+	for {
+		select {
+		case task = <-m.taskCh:
+			go task.Do()
+			count++
+		default:
+			return
+		}
+		if count >= m.conf.MaxInFlightTasks {
+			return
+		}
+	}
 }
--- tests/gossip/test_service.go
+++ tests/gossip/test_service.go
@@ -14,9 +14,9 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
@@ -24,71 +24,29 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/bbva/qed/gossip/member"
-	"github.com/go-redis/redis"
-	"github.com/hashicorp/go-msgpack/codec"
+	"github.com/bbva/qed/protocol"
 )
 
-type stats struct {
+type kv struct {
 	sync.Mutex
-	batch map[string][]int
+	d map[uint64]*protocol.SignedSnapshot
 }
 
-type Digest []byte
+func (kv *kv) Put(b *protocol.BatchSnapshots) {
+	kv.Lock()
+	defer kv.Unlock()
 
-type Snapshot struct {
-	HistoryDigest Digest
-	HyperDigest   Digest
-	Version       uint64
-	EventDigest   Digest
-}
-
-type SignedSnapshot struct {
-	Snapshot  *Snapshot
-	Signature []byte
-}
-
-type BatchSnapshots struct {
-	Snapshots []*SignedSnapshot
-	TTL       int
-	From      *member.Peer
-}
-
-type RedisCli struct {
-	rcli *redis.Client
-}
-
-func NewRedisClient() *RedisCli {
-	c := redis.NewClient(&redis.Options{
-		Addr:     "127.0.0.1:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
-
-	pong, err := c.Ping().Result()
-	if err == nil {
-		fmt.Printf("%s: Redis successfully connected in %s \n", pong, c.Options().Addr)
+	for _, s := range b.Snapshots {
+		kv.d[s.Snapshot.Version] = s
 	}
-	return &RedisCli{rcli: c}
+
 }
 
-func (c *RedisCli) QueueCommands(key string, value []byte) {
-	err := c.rcli.Pipeline().Set(key, value, 0).Err()
-	if err != nil {
-		panic(err)
-	}
+func (kv *kv) Get(version uint64) *protocol.SignedSnapshot {
+	kv.Lock()
+	defer kv.Unlock()
+	return kv.d[version]
 }
-
-func (c *RedisCli) Execute() {
-	// err := c.rcli.Set(key, value, 0).Err()
-	cmds, err := c.rcli.Pipeline().Exec()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(cmds)
-}
-
-// TODO: SeMaaS
 
 func (s *stats) Add(nodeType string, id, v int) {
 	s.Lock()
@@ -97,6 +55,11 @@ func (s *stats) Add(nodeType string, id, v int) {
 		s.batch[nodeType] = make([]int, 10)
 	}
 	s.batch[nodeType][id] += v
+}
+
+type stats struct {
+	sync.Mutex
+	batch map[string][]int
 }
 
 func (s *stats) Get(nodeType string, id int) int {
@@ -119,7 +82,7 @@ func (s *stats) Print() {
 var count uint64
 var s stats
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func statHandler(w http.ResponseWriter, r *http.Request) {
 	q := r.URL.Query()
 	nodeType := q.Get("nodeType")
 	id, _ := strconv.Atoi(q.Get("id"))
@@ -129,39 +92,54 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	atomic.AddUint64(&count, 1)
 }
 
-func PublishHandler(w http.ResponseWriter, r *http.Request, client *RedisCli) {
-	if r.Method == "POST" {
-		// Decode batch to get signed snapshots and batch version.
-		var b BatchSnapshots
-		err := json.NewDecoder(r.Body).Decode(&b)
-		if err != nil {
-			fmt.Println("Error unmarshalling: ", err)
+func putHandler(kv *kv) func(http.ResponseWriter, *http.Request) {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			// Decode batch to get signed snapshots and batch version.
+			var b protocol.BatchSnapshots
+			buf, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			err = b.Decode(buf)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			kv.Put(&b)
+			return
 		}
-
-		// Encode each signed snapshot for sending it to DB.
-		var buf bytes.Buffer
-		encoder := codec.NewEncoder(&buf, &codec.MsgpackHandle{})
-
-		for _, s := range b.Snapshots {
-			key := strconv.FormatUint(s.Snapshot.Version, 10)
-
-			_ = encoder.Encode(s)
-			_ = client.rcli.Set(key, buf.Bytes(), 0).Err()
-
-			// client.QueueCommands(key, buf.Bytes())
-			// if i%len(b.Snapshots) == 0 {
-			// 	go client.Execute()
-			// }
-		}
-
-	} else {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 	}
 }
-
+func getHandler(kv *kv) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			q := r.URL.Query()
+			version, err := strconv.ParseInt(q.Get("v"), 10, 64)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			b := kv.Get(uint64(version))
+			buf, err := b.Encode()
+			_, err = w.Write(buf)
+			if err != nil {
+				fmt.Println("ERROR: %v", err)
+			}
+			return
+		}
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+	}
+}
 func main() {
+	var store kv
+
+	store.d = make(map[uint64]*protocol.SignedSnapshot, 0)
 	s.batch = make(map[string][]int, 0)
-	client := NewRedisClient()
 
 	go func() {
 		ticker := time.NewTicker(2 * time.Second)
@@ -176,7 +154,8 @@ func main() {
 		}
 	}()
 
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/publish", func(w http.ResponseWriter, r *http.Request) { PublishHandler(w, r, client) })
+	http.HandleFunc("/stat", statHandler)
+	http.HandleFunc("/put", putHandler(&store))
+	http.HandleFunc("/get", getHandler(&store))
 	log.Fatal(http.ListenAndServe("127.0.0.1:8888", nil))
 }
