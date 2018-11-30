615c14b08f0984d33e26c4e144bf26226517857d
Martin Evgeniev <martin.evgeniev@bbva.com>
2018-11-30 13:28:38 +0100

    Set new xterm position arount the screen corners
    Former-commit-id: 5c7ab047a2c8a626a01860805eda21bb04ca3591

--- tests/gossip/run_gossip.sh
+++ tests/gossip/run_gossip.sh
@@ -16,25 +16,25 @@ publisher="http://127.0.0.1:8888"
 qed="http://127.0.0.1:8080"
 echo Create id_ed25519 key
 echo -e 'y\n' | ssh-keygen -t ed25519 -N '' -f /var/tmp/id_ed25519
-go run $GOPATH/src/github.com/bbva/qed/main.go start  -k key -l silent --node-id server0 --gossip-addr $master --raft-addr 127.0.0.1:9000 -y /var/tmp/id_ed25519 &
+go run $GOPATH/src/github.com/bbva/qed/main.go start -k key -l silent --node-id server0 --gossip-addr $master --raft-addr 127.0.0.1:9000 -y /var/tmp/id_ed25519 &
 pids[0]=$!
 sleep 1s
 
 for i in `seq 1 $1`;
 do
-	xterm -hold -e "go run $GOPATH/src/github.com/bbva/qed/main.go agent auditor -k key -l info --bind 127.0.0.1:910$i --join $master --endpoints $qed --node auditor$i" &
+	xterm -geometry +0-0 -hold -e "go run $GOPATH/src/github.com/bbva/qed/main.go agent auditor -k key -l info --bind 127.0.0.1:910$i --join $master --endpoints $qed --node auditor$i" &
 	pids+=($!)
 done 
 
 for i in `seq 1 $2`;
 do
-	xterm -hold -e "go run $GOPATH/src/github.com/bbva/qed/main.go agent monitor -k key -l info --bind 127.0.0.1:920$i --join $master --endpoints $qed --node monitor$i" &
+	xterm -geometry -0+0 -hold -e "go run $GOPATH/src/github.com/bbva/qed/main.go agent monitor -k key -l info --bind 127.0.0.1:920$i --join $master --endpoints $qed --node monitor$i" &
 	pids+=($!)
 done 
 
 for i in `seq 1 $3`;
 do
-	xterm -hold -e "go run $GOPATH/src/github.com/bbva/qed/main.go agent publisher -k key -l info --bind 127.0.0.1:930$i --join $master --endpoints $publisher --node publisher$i" &
+	xterm -geometry -0-0 -hold -e "go run $GOPATH/src/github.com/bbva/qed/main.go agent publisher -k key -l info --bind 127.0.0.1:930$i --join $master --endpoints $publisher --node publisher$i" &
 	pids+=($!)
 done 
 
