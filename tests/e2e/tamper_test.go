1427a6732fb60533990a38c18fd524e5b95ec04e
Jose Luis Lucas <joseluis.lucas.simarro@bbva.com>
2018-12-13 09:37:21 +0100

    WIP: Improve tamper e2e test.
    Co-authored-by: iknite <enrique@iknite.com>
    
    Former-commit-id: d2a3f18b5440121066d75cb57da9205cf6b0825a [formerly b71e16c8c0535beb64854542ea3827bcbe862f2e]
    Former-commit-id: 3a13f3e8abb2ebbb5e1254b360edcf3c6c4e00c2

--- tests/e2e/tamper_test.go
+++ tests/e2e/tamper_test.go
@@ -16,35 +16,104 @@
 package e2e
 
 import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
 	"testing"
+	"time"
 
+	"github.com/bbva/qed/hashing"
+	"github.com/bbva/qed/protocol"
 	"github.com/bbva/qed/testutils/rand"
 	"github.com/bbva/qed/testutils/scope"
 	assert "github.com/stretchr/testify/require"
 )
 
+func getSnapshot(version uint64) (*protocol.SignedSnapshot, error) {
+	resp, err := http.Get(fmt.Sprintf("%s/snapshot?v=%d", PubUrl, version))
+	if err != nil {
+		return nil, fmt.Errorf("Error getting snapshot from the store: %v", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Error getting snapshot from the store. Status: %d", resp.StatusCode)
+	}
+
+	buf, _ := ioutil.ReadAll(resp.Body)
+	s := &protocol.SignedSnapshot{}
+	err = s.Decode(buf)
+	if err != nil {
+		return nil, fmt.Errorf("Error decoding signed snapshot %d codec", version)
+	}
+	return s, nil
+}
+
 func TestTamper(t *testing.T) {
 	bStore, aStore := setupStore(t)
 	bServer, aServer := setupServer(0, "", t)
-	bAuditor, aAuditor := setupAuditor(0, t)
-	bMonitor, aMonitor := setupMonitor(0, t)
+	// bAuditor, aAuditor := setupAuditor(0, t)
+	// bMonitor, aMonitor := setupMonitor(0, t)
 	bPublisher, aPublisher := setupPublisher(0, t)
 
 	scenario, let := scope.Scope(t,
-		merge(bStore, bServer, bAuditor, bMonitor, bPublisher),
-		merge(aStore, aServer, aAuditor, aMonitor, aPublisher),
+		merge(bStore, bServer /*bAuditor, bMonitor*/, bPublisher),
+		merge( /*aAuditor, aMonitor,*/ aPublisher, aServer, aStore),
 	)
 
 	client := getClient(0)
 
 	event := rand.RandomString(10)
 
-	scenario("S", func() {
+	scenario("Add one event and get its membership proof", func() {
+		var snapshot *protocol.Snapshot
 		var err error
 
 		let("Add event", func(t *testing.T) {
-			_, err = client.Add(event)
+			snapshot, err = client.Add(event)
 			assert.NoError(t, err)
+
+			assert.Equal(t, snapshot.EventDigest, hashing.NewSha256Hasher().Do([]byte(event)),
+				"The snapshot's event doesn't match: expected %s, actual %s", event, snapshot.EventDigest)
+			assert.False(t, snapshot.Version < 0, "The snapshot's version must be greater or equal to 0")
+			assert.False(t, len(snapshot.HyperDigest) == 0, "The snapshot's hyperDigest cannot be empty")
+			assert.False(t, len(snapshot.HistoryDigest) == 0, "The snapshot's hyperDigest cannot be empt")
+		})
+
+		let("Get membership proof for first inserted event", func(t *testing.T) {
+			result, err := client.Membership([]byte(event), snapshot.Version)
+			assert.NoError(t, err)
+
+			assert.True(t, result.Exists, "The queried key should be a member")
+			assert.Equal(t, result.QueryVersion, snapshot.Version,
+				"The query version doest't match the queried one: expected %d, actual %d", snapshot.Version, result.QueryVersion)
+			assert.Equal(t, result.ActualVersion, snapshot.Version,
+				"The actual version should match the queried one: expected %d, actual %d", snapshot.Version, result.ActualVersion)
+			assert.Equal(t, result.CurrentVersion, snapshot.Version,
+				"The current version should match the queried one: expected %d, actual %d", snapshot.Version, result.CurrentVersion)
+			assert.Equal(t, []byte(event), result.Key,
+				"The returned event doesn't math the original one: expected %s, actual %s", event, result.Key)
+			assert.False(t, len(result.KeyDigest) == 0, "The key digest cannot be empty")
+			assert.False(t, len(result.Hyper) == 0, "The hyper proof cannot be empty")
+			assert.False(t, result.ActualVersion > 0 && len(result.History) == 0,
+				"The history proof cannot be empty when version is greater than 0")
+
 		})
 	})
+
+	scenario("S", func() {
+		var snapshot *protocol.Snapshot
+		var err error
+
+		let("Add event", func(t *testing.T) {
+			snapshot, err = client.Add(event)
+			assert.NoError(t, err)
+			time.Sleep(100 * time.Second)
+			ss, err := getSnapshot(0)
+			if err != nil {
+				fmt.Println("Error: ", err)
+			}
+			assert.Equal(t, snapshot, ss.Snapshot)
+		})
+	})
+
 }
