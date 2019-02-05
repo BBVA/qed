5f47bdc9b556038dc1ca9286cc7562f27b627d3b
Martin Evgeniev <martin.evgeniev@bbva.com>
2019-02-05 11:42:36 +0100

    Store terraform state in S3

---
+++ deploy/aws/pre-build/bucket.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 0.11.11"
+}
+
+provider "aws" {
+  version = ">= 1.56.0, < 2.0"
+  region = "eu-west-1"
+  profile = "${var.aws_profile}"
+}
+resource "aws_s3_bucket" "terraform-qed-cluster" {
+    bucket = "terraform-qed-cluster"
+ 
+    versioning {
+      enabled = true
+    }
+ 
+    lifecycle {
+      prevent_destroy = true
+    }
+ 
+    tags {
+      Name = "S3 Remote Terraform State Store"
+    }      
+}
