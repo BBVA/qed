e38dde66fcfc2c1f6bc1a8edadaa1ac5ea6672b2
Martin Evgeniev <martin.evgeniev@bbva.com>
2019-02-11 16:14:47 +0100

    Fix: deploy modules and print outputs
    Former-commit-id: 6026e41c39383cc67d8a8ab3cb471aaf2da2b5b3

--- deploy/aws/outputs.tf
+++ deploy/aws/outputs.tf
@@ -29,3 +29,20 @@ output "riot" {
   value = "${module.riot.public_ip}"
 }
 
+output "inmemory-storage" {
+  value = "${module.inmemory-storage.public_ip}"
+}
+
+output "agent-publisher" {
+  value = "${module.agent-publisher.public_ip}"
+}
+
+output "agent-monitor" {
+  value = "${module.agent-monitor.public_ip}"
+}
+
+output "agent-auditor" {
+  value = "${module.agent-auditor.public_ip}"
+}
+
+
