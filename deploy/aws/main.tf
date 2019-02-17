b296d1900b42a06ddcac7a0eb527b67edc0c8a23
Martin Evgeniev <martin.evgeniev@bbva.com>
2019-02-17 15:45:07 +0100

    Remove terraform prometheus template config
    Former-commit-id: 1d2877960db68ff79319ce73ebb721fc33d4f3ce

--- deploy/aws/main.tf
+++ deploy/aws/main.tf
@@ -88,65 +88,6 @@ module "prometheus" {
   subnet_id = "${element(data.aws_subnet_ids.all.ids, 0)}"
   key_name = "${aws_key_pair.qed.key_name}"
   key_path = "${var.keypath}"
-
-  # config = <<-CONFIG
-  # global:
-  #   scrape_interval:     15s
-  #   evaluation_interval: 15s
-  # scrape_configs:
-  #   - job_name: 'prometheus'
-  #     scrape_interval: 5s
-  #     static_configs:
-  #       - targets: ['localhost:9090']
-  #   - job_name: 'Qed0-HostMetrics'
-  #     scrape_interval: 10s
-  #     static_configs:
-  #       - targets: ['${module.leader.private_ip[0]}:9100']
-  #   - job_name: 'Qed0-QedMetrics'
-  #     scrape_interval: 10s
-  #     static_configs:
-  #       - targets: ['${module.leader.private_ip[0]}:8600']
-  #   - job_name: 'Qed1-HostMetrics'
-  #     scrape_interval: 10s
-  #     static_configs:
-  #       - targets: ['${module.follower-1.private_ip[0]}:9100']
-  #   - job_name: 'Qed1-QedMetrics'
-  #     scrape_interval: 10s
-  #     static_configs:
-  #       - targets: ['${module.follower-1.private_ip[0]}:8600']
-  #   - job_name: 'Qed2-HostMetrics'
-  #     scrape_interval: 10s
-  #     static_configs:
-  #       - targets: ['${module.follower-2.private_ip[0]}:9100']
-  #   - job_name: 'Qed2-QedMetrics'
-  #     scrape_interval: 10s
-  #     static_configs:
-  #       - targets: ['${module.follower-2.private_ip[0]}:8600']
-  #   - job_name: 'Agent-Publisher-Metrics'
-  #     scrape_interval: 10s
-  #     static_configs:
-  #       - targets: ['${module.agent-publisher.private_ip[0]}:18300']
-  #   - job_name: 'Agent-Monitor-0-Metrics'
-  #     scrape_interval: 10s
-  #     static_configs:
-  #       - targets: ['${module.agent-monitor.private_ip[0]}:18200']
-  #   - job_name: 'Agent-Monitor-1-Metrics'
-  #     scrape_interval: 10s
-  #     static_configs:
-  #       - targets: ['${module.agent-monitor.private_ip[1]}:18200']
-  #   - job_name: 'Agent-Auditor-Metrics'
-  #     scrape_interval: 10s
-  #     static_configs:
-  #       - targets: ['${module.agent-auditor.private_ip[0]}:18100']
-  #   - job_name: 'riot'
-  #     scrape_interval: 10s
-  #     static_configs:
-  #       - targets: ['${module.riot.private_ip}:9100']
-  #   - job_name: 'inmemory-storage'
-  #     scrape_interval: 10s
-  #     static_configs:
-  #       - targets: ['${module.inmemory-storage.private_ip}:18888']
-  # CONFIG
 }
 
 module "riot" {
