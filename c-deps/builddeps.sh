a1d9da2203a028e44e883a8149807d4e3445c2f0
Gabriel Diaz <g.diaz.lopezllave@bbva.com>
2019-02-27 17:38:58 +0100

    build rocksdb using static_lib target
    Former-commit-id: c510cf060c81a24950034ef85b5379a72a955c65 [formerly f230aa91a17193e362d31b90bb378a8e4b5a7aa2]
    Former-commit-id: 51d4ba2fad60ca05805a15b97fca347f5ffabaec

--- c-deps/builddeps.sh
+++ c-deps/builddeps.sh
@@ -4,9 +4,10 @@ cd rocksdb
 DEBUG_LEVEL=0
 PORTABLE=1
 
-for lib in libbz2.a liblz4.a librocksdb.a libsnappy.a libz.a libzstd.a; do
+for lib in libbz2.a liblz4.a libsnappy.a libz.a libzstd.a; do
 	make $lib
 done
+make static_lib
 strip *.a
 cd ..
 
