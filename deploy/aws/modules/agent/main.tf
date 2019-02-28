79f1d2526bdbe14bf46512f3867b14760a27a5e1
pancho horrillo <pedrofelipe.horrillo@bbva.com>
2019-02-28 18:31:25 +0100

    AWS deploy: split qed terraform module in two (qed, agent)
    This is to prepare the qed module to configure an EBS volume.

---
+++ deploy/aws/modules/agent/main.tf
@@ -0,0 +1,49 @@
+#  Copyright 2018 Banco Bilbao Vizcaya Argentaria, S.A.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+}
+
+resource "aws_instance" "qed-agent" {
+  count                = "${var.count}"
+  ami                  = "${data.aws_ami.amazon_linux.id}"
+  instance_type        = "${var.instance_type}"
+  iam_instance_profile = "${var.iam_instance_profile}"
+
+  vpc_security_group_ids      = ["${var.vpc_security_group_ids}"]
+  subnet_id                   = "${var.subnet_id}"
+  associate_public_ip_address = true
+  key_name                    = "${var.key_name}"
+
+  root_block_device = [{
+    volume_type = "gp2"
+    volume_size = "${var.volume_size}"
+  }]
+
+  tags {
+    Name = "${format("${var.name}-%01d", count.index)}"
+    Role = "${var.role}"
+  }
+}
