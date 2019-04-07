500dd49a7cccab1f7536657a836527a7b557b2b9
Gabriel Díaz <g.diaz.lopezllave@bbva.com>
2019-04-07 23:57:07 +0200

    Redesign cli flag parsing. Use new library to generate options from config objects
    Former-commit-id: 487dd8f4e61251646f3e671b5faa64be13b302fc [formerly 40d10bb4c09d65f0b1cf818e819a51575ffde069]
    Former-commit-id: f2630358e7d09d6bef8cb34638544279a2f09bf5

--- cmd/agent_auditor.go
+++ cmd/agent_auditor.go
@@ -3,7 +3,9 @@
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
+
        http://www.apache.org/licenses/LICENSE-2.0
+
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS,
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,74 +16,21 @@
 package cmd
 
 import (
-	"github.com/spf13/cobra"
-	v "github.com/spf13/viper"
+	"context"
 
-	"github.com/bbva/qed/gossip"
-	"github.com/bbva/qed/gossip/auditor"
-	"github.com/bbva/qed/gossip/member"
-	"github.com/bbva/qed/log"
-	"github.com/bbva/qed/metrics"
-	"github.com/bbva/qed/util"
+	"github.com/spf13/cobra"
 )
 
-func newAgentAuditorCommand(ctx *cmdContext, config gossip.Config, agentPreRun func(gossip.Config) gossip.Config) *cobra.Command {
-
-	auditorConfig := auditor.DefaultConfig()
+func newAgentAuditorCommand(ctx context.Context, args []string) *cobra.Command {
 
 	cmd := &cobra.Command{
 		Use:   "auditor",
 		Short: "Start a QED auditor",
 		Long:  `Start a QED auditor that reacts to snapshot batches propagated by QED servers and periodically executes membership queries to verify the inclusion of events`,
-		PreRun: func(cmd *cobra.Command, args []string) {
-
-			log.SetLogger("QEDAuditor", ctx.logLevel)
-
-			// WARN: PersitentPreRun can't be nested and we're using it in cmd/root so inbetween preRuns
-			// must be curried.
-			config = agentPreRun(config)
-
-			auditorConfig.QEDUrls = v.GetStringSlice("agent.server_urls")
-			auditorConfig.PubUrls = v.GetStringSlice("agent.snapshots_store_urls")
-			auditorConfig.AlertsUrls = v.GetStringSlice("agent.alerts_urls")
-
-			markSliceStringRequired(auditorConfig.QEDUrls, "qedUrls")
-			markSliceStringRequired(auditorConfig.PubUrls, "pubUrls")
-			markSliceStringRequired(auditorConfig.AlertsUrls, "alertsUrls")
-		},
 		Run: func(cmd *cobra.Command, args []string) {
 
-			config.Role = member.Auditor
-			auditorConfig.APIKey = ctx.apiKey
-
-			auditor, err := auditor.NewAuditor(*auditorConfig)
-			if err != nil {
-				log.Fatalf("Failed to start the QED monitor: %v", err)
-			}
-			metricsServer := metrics.NewServer(config.MetricsAddr)
-			agent, err := gossip.NewAgent(&config, []gossip.Processor{auditor}, metricsServer)
-			if err != nil {
-				log.Fatalf("Failed to start the QED auditor: %v", err)
-			}
-
-			contacted, err := agent.Join(config.StartJoin)
-			if err != nil {
-				log.Fatalf("Failed to join the cluster: %v", err)
-			}
-			log.Debugf("Number of nodes contacted: %d (%v)", contacted, config.StartJoin)
-
-			defer agent.Shutdown()
-			util.AwaitTermSignal(agent.Leave)
 		},
 	}
 
-	f := cmd.Flags()
-	f.StringSliceVarP(&auditorConfig.QEDUrls, "qedUrls", "", []string{}, "Comma-delimited list of QED servers ([host]:port), through which an auditor can make queries")
-	f.StringSliceVarP(&auditorConfig.PubUrls, "pubUrls", "", []string{}, "Comma-delimited list of store servers ([host]:port), through which an auditor can make queries")
-	f.StringSliceVarP(&auditorConfig.AlertsUrls, "alertsUrls", "", []string{}, "Comma-delimited list of alerts servers ([host]:port), through which an auditor can make queries")
-	// Lookups
-	v.BindPFlag("agent.server_urls", f.Lookup("qedUrls"))
-	v.BindPFlag("agent.snapshots_store_urls", f.Lookup("pubUrls"))
-	v.BindPFlag("agent.alerts_urls", f.Lookup("alertsUrls"))
 	return cmd
 }
--- cmd/agent_publisher.go
+++ cmd/agent_publisher.go
@@ -3,7 +3,9 @@
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
+
        http://www.apache.org/licenses/LICENSE-2.0
+
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS,
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,73 +16,20 @@
 package cmd
 
 import (
-	"github.com/spf13/cobra"
-	v "github.com/spf13/viper"
+	"context"
 
-	"github.com/bbva/qed/gossip"
-	"github.com/bbva/qed/gossip/member"
-	"github.com/bbva/qed/gossip/publisher"
-	"github.com/bbva/qed/log"
-	"github.com/bbva/qed/metrics"
-	"github.com/bbva/qed/util"
+	"github.com/spf13/cobra"
 )
 
-func newAgentPublisherCommand(ctx *cmdContext, config gossip.Config, agentPreRun func(gossip.Config) gossip.Config) *cobra.Command {
-
-	publisherConfig := publisher.DefaultConfig()
+func newAgentPublisherCommand(ctx context.Context, args []string) *cobra.Command {
 
 	cmd := &cobra.Command{
 		Use:   "publisher",
 		Short: "Start a QED publisher",
 		Long:  `Start a QED publisher that reacts to snapshot batches propagated by QED servers and periodically publishes them to a certain log storage.`,
-		PreRun: func(cmd *cobra.Command, args []string) {
-
-			log.SetLogger("QEDPublisher", ctx.logLevel)
-
-			// WARN: PersitentPreRun can't be nested and we're using it in
-			// cmd/root so inbetween preRuns must be curried.
-			config = agentPreRun(config)
-
-			// Bindings
-			publisherConfig.MetricsAddr = config.BindAddr // TODO: make MetricsAddr configurable
-			publisherConfig.PubUrls = v.GetStringSlice("agent.snapshots_store_urls")
-			publisherConfig.AlertsUrls = v.GetStringSlice("agent.alerts_urls")
-
-			markSliceStringRequired(publisherConfig.PubUrls, "pubUrls")
-			markSliceStringRequired(publisherConfig.AlertsUrls, "alertsUrls")
-		},
 		Run: func(cmd *cobra.Command, args []string) {
-
-			config.Role = member.Publisher
-
-			publisher, err := publisher.NewPublisher(*publisherConfig)
-			if err != nil {
-				log.Fatalf("Failed to start the QED publisher: %v", err)
-			}
-			metricsServer := metrics.NewServer(config.MetricsAddr)
-			agent, err := gossip.NewAgent(&config, []gossip.Processor{publisher}, metricsServer)
-			if err != nil {
-				log.Fatalf("Failed to start the QED publisher: %v", err)
-			}
-
-			contacted, err := agent.Join(config.StartJoin)
-			if err != nil {
-				log.Fatalf("Failed to join the cluster: %v", err)
-			}
-			log.Debugf("Number of nodes contacted: %d", contacted)
-
-			defer agent.Shutdown()
-			util.AwaitTermSignal(agent.Leave)
 		},
 	}
-
-	f := cmd.Flags()
-	f.StringSliceVarP(&publisherConfig.PubUrls, "pubUrls", "", []string{}, "Comma-delimited list of end-publishers ([host]:port), through which an publisher can send requests")
-	f.StringSliceVarP(&publisherConfig.AlertsUrls, "alertsUrls", "", []string{}, "Comma-delimited list of QED servers ([host]:port), through which an monitor can publish alerts")
-
-	// Lookups
-	v.BindPFlag("agent.snapshots_store_urls", f.Lookup("pubUrls"))
-	v.BindPFlag("agent.alerts_urls", f.Lookup("alertsUrls"))
 
 	return cmd
 }
