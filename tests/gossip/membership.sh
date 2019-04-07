b7d8519029ff9ccef495166c0bb21cc41e5eeeb4
Gabriel Díaz <g.diaz.lopezllave@bbva.com>
2019-04-07 23:58:04 +0200

    Update scripts to launch qed components
    Former-commit-id: 1231310fcac7f184f4701e7e74decb9e7e4f4c76 [formerly 6ec665d92441911f293deb321cf89d0a2ef95475]
    Former-commit-id: 1f810d119728d446f4bbb2f4bf53a79bde3658dd

--- tests/gossip/membership.sh
+++ tests/gossip/membership.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
+# client options
+CLIENT_CONFIG=()
+CLIENT_CONFIG+=("--log debug")
+CLIENT_CONFIG+=("--endpoints http://127.0.0.1:8800")
+config=$(echo ${CLIENT_CONFIG[@]} | i=0 envsubst )
 
-go run $GOPATH/src/github.com/bbva/qed/main.go 	\
-	--apikey my-key \
-	client \
-		--log info \
-		--endpoints http://${QED_LEADER}:8800 \
-		membership --key key$1 \
-		--version $1 --verify
+go run $GOPATH/src/github.com/bbva/qed/main.go client membership $config --event $1
