e86fec6fda169d34e73c8b019f07643695022860
Gabriel Díaz <g.diaz.lopezllave@bbva.com>
2019-04-09 22:09:17 +0200

    MOve test_service to notifierstore in testutils

--- testutils/notifierstore.go
+++ testutils/notifierstore.go
@@ -3,7 +3,9 @@
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
+
        http://www.apache.org/licenses/LICENSE-2.0
+
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS,
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,10 +15,10 @@
 
 package main
 
-import "github.com/bbva/qed/tests/e2e"
+import "github.com/bbva/qed/testutils/notifierstore"
 
 func main() {
-	s := e2e.NewService()
+	s := notifierstore.NewService()
 	foreground := true
 	s.Start(foreground)
 }
