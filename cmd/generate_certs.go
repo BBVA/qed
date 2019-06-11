2998d37f2d4af02b07f9df3d97a4f429ecb6a155
Martin Evgeniev <suizman@outlook.com>
2019-06-11 15:26:25 +0200

    Move keys package to crypto
    Also generate new tlscerts command.
    
    
    Former-commit-id: 1c825f4dcd6ae0035eb586f6b6cf70c9e1eaf2aa

---
+++ cmd/generate_certs.go
@@ -0,0 +1,46 @@
+/*
+   Copyright 2018-2019 Banco Bilbao Vizcaya Argentaria, S.A.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/bbva/qed/crypto"
+	"github.com/spf13/cobra"
+)
+
+var generateTlsCerts *cobra.Command = &cobra.Command{
+	Use:   "tlscerts",
+	Short: "Generate Tls Certificates",
+	Run:   runGenerateTlsCerts,
+}
+
+func init() {
+	generateCmd.AddCommand(generateTlsCerts)
+}
+
+func runGenerateTlsCerts(cmd *cobra.Command, args []string) {
+
+	conf := generateCtx.Value(k("generate.config")).(*GenerateConfig)
+
+	certs, err := crypto.NewTlsCerts(conf.Path)
+	if err != nil {
+		fmt.Errorf("Error: %v\n", err)
+	}
+	fmt.Printf("New Tls certificates generated at:\n%v\n", certs)
+
+}
