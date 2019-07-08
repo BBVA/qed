0d8e88b1f5379bf870d20235e6b42dcedf2e997d
Martin Evgeniev <suizman@outlook.com>
2019-07-08 16:13:20 +0200

    Merge cmd/bug.go and debug.go files

--- cmd/bug.go
+++ cmd/bug.go
@@ -20,6 +20,7 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"io"
 	"net/url"
 	"os"
 	"os/exec"
@@ -31,6 +32,53 @@ import (
 	"github.com/octago/sflags/gen/gpflag"
 	"github.com/spf13/cobra"
 )
+
+func formatInfo(w io.Writer, title string, body func() error) error {
+	fmt.Fprintf(w, "#### %s\n\n```\n", title)
+	err := body()
+	if err != nil {
+		return err
+	}
+	fmt.Fprintf(w, "```\n")
+	return nil
+}
+
+func debugInfo(w io.Writer) error {
+	var err error
+	qedVersion := Ctx.Value(k("version"))
+	// Get build version
+	formatInfo(w, "Build Info", func() error {
+		fmt.Fprintf(w, "QED version %s, built in $GOPATH mode\n", qedVersion)
+		return nil
+	})
+
+	// Get GO environment info
+	err = formatInfo(w, "Go Info", func() error {
+		gov := exec.Command("go", "version")
+		env := exec.Command("go", "env")
+		gov.Stdout = w
+		env.Stdout = w
+
+		err = gov.Run()
+		if err != nil {
+			return fmt.Errorf("Getting Go version: %v", err)
+		}
+
+		err = env.Run()
+		if err != nil {
+			return fmt.Errorf("Getting Go environment: %v", err)
+		}
+
+		fmt.Fprint(w)
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 // Commands returns a list of commands to use to open a url.
 func Commands() [][]string {
