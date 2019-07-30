fbe45d5e9adfe4d37f6575397aad0806685fda0e
Jose Luis Lucas <joseluis.lucas.simarro@bbva.com>
2019-07-30 10:29:51 +0200

    Fix: backup test
    Former-commit-id: bd63f75291fc13b436a1a7f20fd69df8fb0877a5 [formerly ced50f99f26b252ae7fc1ac5942627df2ae16ff4]
    Former-commit-id: e3e94e33fe517c4cfca426b35c0fa28bb17d8905

--- tests/e2e/setup_for_test.go
+++ tests/e2e/setup_for_test.go
@@ -19,7 +19,6 @@ package e2e
 import (
 	"crypto/tls"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"os"
 	"runtime/debug"
@@ -137,8 +136,8 @@ func newServerSetup(id int, tls bool) (func() (string, error), func() error) {
 	var err error
 
 	before := func() (string, error) {
-		// path = fmt.Sprintf("/var/tmp/e2e-qed%d", id)
-		path, err = ioutil.TempDir("/var/tmp", "e2e-qed-") // os.MkdirAll(path, os.ModePerm)
+		path = fmt.Sprintf("/var/tmp/e2e-qed%d", id)
+		err = os.MkdirAll(path, os.ModePerm)
 		if err != nil {
 			return "", err
 		}
