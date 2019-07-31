a3f3cb71685869539e54d5472bfcb25d6cbae64e
Gabriel Diaz <g.diaz.lopezllave@bbva.com>
2019-07-31 16:43:34 +0200

    add log option to testing servers
    Former-commit-id: e6ce9ff4c2cdeeeb4222cc8335f7ebfa3d6474f3

--- tests/e2e/setup_for_test.go
+++ tests/e2e/setup_for_test.go
@@ -29,6 +29,7 @@ import (
 	"github.com/bbva/qed/client"
 	"github.com/bbva/qed/crypto"
 	"github.com/bbva/qed/crypto/hashing"
+	"github.com/bbva/qed/log"
 	"github.com/bbva/qed/server"
 	"github.com/bbva/qed/testutils/notifierstore"
 	"github.com/bbva/qed/testutils/scope"
@@ -103,6 +104,7 @@ func configQedServer(id int, pathDB, signPath, tlsPath string, tls bool) *server
 	hostname, _ := os.Hostname()
 	conf := server.DefaultConfig()
 	conf.APIKey = "APIKey"
+	conf.Log = log.SILENT
 	conf.NodeID = fmt.Sprintf("%s-%d", hostname, id)
 	conf.HTTPAddr = fmt.Sprintf("127.0.0.1:880%d", id)
 	conf.MgmtAddr = fmt.Sprintf("127.0.0.1:870%d", id)
